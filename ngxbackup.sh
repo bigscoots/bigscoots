c839ba4d7809e7a8a1a01c5873366168e5d5a5ad
Justin catello <justin@clientexec.com>
2016-05-27 06:55:04 -0400

    backupscript

---
+++ ngxbackup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOCROOT=/home/nginx/domains/fairpoint.com/public
+DB=$(grep DB_NAME $DOCROOT/wp-config.php | sed -e "s/define('DB_NAME', '//g" -e "s/');//g")
+SITE=$(echo "$DOCROOT" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
+
+mkdir -p /backup/DATE="$(date +%Y-%m-%d)/$SITE"
+mysqldump "$DB" | gzip > $DOCROOT/"$DB".sql.gz 
+tar -zcf /backup/DATE="$(date +%Y-%m-%d)"/"$SITE".tar.gz "$DOCROOT"
+rm -f $DOCROOT/"$DB".sql.gz
+find /backup/ -type f -iname '*.tar.gz' ctime +14 -exec rm {} \;
