be9e656b6d04d0826c22fbd0dcea09eb622e5a3b
Justin catello <justin@clientexec.com>
2016-06-03 20:51:58 -0400

    Update ngxbackup.sh

--- ngxbackup.sh
+++ ngxbackup.sh
@@ -5,12 +5,12 @@ DOCROOT=/home/nginx/domains/domain.com/public
 DB=$(grep DB_NAME $DOCROOT/wp-config.php | sed -e "s/define('DB_NAME', '//g" -e "s/');//g" | tr -d '\r')
 SITE=$(echo "$DOCROOT" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
 
-ssh "$BKUSER"@int-backup.bigscoots.com 'mkdir -p ~/backup/"$(date +%Y-%m-%d)"'
+ssh "$BKUSER"@int-backup2.bigscoots.com 'mkdir -p ~/backup/"$(date +%Y-%m-%d)"'
 
 mysqldump "$DB" | gzip > $DOCROOT/"$DB".sql.gz
 
-tar zcf - "$DOCROOT" | ssh "$BKUSER"@int-backup.bigscoots.com "cat > ~/backup/$(date +%Y-%m-%d)/$SITE.tar.gz"
+tar zcf - "$DOCROOT" | ssh "$BKUSER"@int-backup2.bigscoots.com "cat > ~/backup/$(date +%Y-%m-%d)/$SITE.tar.gz"
 
 rm -f $DOCROOT/"$DB".sql.gz
 
-ssh "$BKUSER"@int-backup.bigscoots.com 'find ~/backup/ -type d -ctime +3 -exec rm -rf {} \;'
+ssh "$BKUSER"@int-backup2.bigscoots.com 'find ~/backup/ -type d -ctime +3 -exec rm -rf {} \;'
