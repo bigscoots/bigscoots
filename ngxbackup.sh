12a9203e9c4a1c615d7fc95ff6ff7bd2ecf921ba
Justin catello <justin@clientexec.com>
2016-07-05 22:24:38 -0400

    Update ngxbackup.sh

--- ngxbackup.sh
+++ ngxbackup.sh
@@ -8,21 +8,22 @@ FREQUENCY=$2
 DBSERVER=int-backup2.bigscoots.com
 
 # No Touch
-DB=$(grep DB_NAME "$DOCROOT"/wp-config.php | sed -e "s/define('DB_NAME', '//g" -e "s/');//g" | tr -d '\r')
+DB=$(grep DB_NAME "$DOCROOT"/wp-config.php | grep -Ev '//define' | sed -e "s/define('DB_NAME', '//g" -e "s/');//g" | tr -d '\r')
 SITE=$(echo "$DOCROOT" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
 CURRDATE=$(date +%Y-%m-%d)
+MYSQLDUMP=$(which mysqldump)
 
 # Remove old backups
-ssh "$BKUSER"@$DBSERVER 'find ~/backup/$FREQUENCY/ -type d -mmin +360 -exec rm -rf {} \;'
+ssh "$BKUSER"@$DBSERVER 'find ~/backup/$FREQUENCY/ -type d -ctime +25 -exec rm -rf {} \;'
 
 # Create the backup directory on the backup server
 ssh "$BKUSER"@$DBSERVER "mkdir -p ~/backup/$FREQUENCY/$CURRDATE"
 
 # Backup the database into the sites docroot
-mysqldump "$DB" | gzip > "$DOCROOT"/"$DB".sql.gz
+$MYSQLDUMP "$DB" | gzip > "$DOCROOT"/"$DB".sql.gz
 
 # Tarbal the entire site including database directly onto the backup server
-tar zcf - "$DOCROOT" | ssh "$BKUSER"@$DBSERVER "cat > ~/backup/$FREQUENCY/$CURRDATE/$SITE.tar.gz"
+tar zvcf - "$DOCROOT" --exclude='cache' | ssh "$BKUSER"@$DBSERVER "cat > ~/backup/$FREQUENCY/$CURRDATE/$SITE.tar.gz"
 
 # Remove the database backup from the sites docroot
 rm -f "$DOCROOT"/"$DB".sql.gz
