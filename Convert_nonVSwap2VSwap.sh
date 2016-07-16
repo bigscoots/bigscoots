18d2eb87151806aa811024dbdc8fc7206a9b2548
Justin catello <justin@clientexec.com>
2016-07-16 12:25:35 -0400

    Update Convert_nonVSwap2VSwap.sh

--- Convert_nonVSwap2VSwap.sh
+++ Convert_nonVSwap2VSwap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Requires bc
 # Usage single: /bigscoots/Convert_nonVSwap2VSwap.sh 123
 # Usage all: for CT in $(vzlist -H -o ctid); /bigscoots/Convert_nonVSwap2VSwap.sh $CT ; done
 
 CTID=$1
 CURRENTMEM=$(vzctl exec $CTID free -m | grep Mem: | awk '{print $2}')
-RAM="$(($CURRENTMEM / 1024))G"
-SWAP="$(($CURRENTMEM / 1024 * 2))G"
+RAM="$(echo "scale=2;$CURRENTMEM/1024" | bc -l)G"
+SWAP="$(echo "scale=2;$CURRENTMEM/1024*2" | bc -l)G"
 CFG=/etc/vz/conf/${CTID}.conf
 
 echo "CTID = $CTID"
@@ -14,7 +15,7 @@ echo "RAM = $RAM"
 echo "SWAP = $SWAP"
 
 cp $CFG $CFG.pre-vswap
-grep -Ev '^(KMEMSIZE|LOCKEDPAGES|PRIVVMPAGES|SHMPAGES|NUMPROC|PHYSPAGES|VMGUARPAGES|OOMGUARPAGES|NUMTCPSOCK|NUMFLOCK|NUMPTY|NUMSIGINFO|TCPSNDBUF|TCPRCVBUF|OTHERSOCKBUF|DGRAMRCVBUF|NUMOTHERSOCK|DCACHESIZE|NUMFILE|AVNUMPROC|NUMIPTENT|ORIGIN_SAMPLE|SWAPPAGES)=' > $CFG <  $CFG.pre-vswap
+grep -Ev '^(KMEMSIZE|LOCKEDPAGES|PRIVVMPAGES|SHMPAGES|NUMPROC|PHYSPAGES|VMGUARPAGES|OOMGUARPAGES|NUMTCPSOCK|NUMFLOCK|NUMPTY|NUMSIGINFO|TCPSNDBUF|TCPRCVBUF|O$
 vzctl set $CTID --ram $RAM --swap $SWAP --save
 vzctl set $CTID --reset_ub
 
