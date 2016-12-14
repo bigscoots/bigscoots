afe22bb1334ee4f53e12b2b1011dc71c5e1f4177
Justin catello <justin@clientexec.com>
2016-12-14 09:48:42 -0500

    Update ngxbackup.sh

--- ngxbackup.sh
+++ ngxbackup.sh
@@ -8,7 +8,7 @@ FREQUENCY=$2
 DBSERVER=int-backup2.bigscoots.com
 
 # No Touch
-DB=$(grep DB_NAME "$DOCROOT"/wp-config.php | grep -Ev '//define' | sed -e "s/define('DB_NAME', '//g" -e "s/');//g" | tr -d '\r')
+DB=$(grep DB_NAME "$DOCROOT"/wp-config.php | grep -Ev '//define|WP_CACHE_KEY_SALT' | sed -e "s/define('DB_NAME', '//g" -e "s/');//g" | tr -d '\r')
 SITE=$(echo "$DOCROOT" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
 CURRDATE=$(date +%Y-%m-%d)
 MYSQLDUMP=$(which mysqldump)
