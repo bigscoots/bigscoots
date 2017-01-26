22289ad4a17190edf99caf838e34fc6632297380
Justin catello <justin@clientexec.com>
2017-01-25 21:21:03 -0500

    Update mon_disk.sh

--- mon_disk.sh
+++ mon_disk.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-df -H | grep -vE '^Filesystem|tmpfs|cdrom|none' | awk '{ print $5 " " $1 }' | while read -r output;
+df -H | grep -vE '^Filesystem|tmpfs|cdrom|none' | awk '{ print $5 " " $1 }' | sort | uniq | while read -r output;
 do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo "$output" | awk '{ print $2 }' )
-  if [ "$usep" -ge 90 ]; then
+  if [ "$usep" -ge 95 ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
      mail -s "Alert: Almost out of disk space $usep%" monitor@bigscoots.com
   fi
