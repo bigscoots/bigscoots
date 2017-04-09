bbd1e389c695ff796cd1670a2100a118b757bce6
Justin catello <justin@bigscoots.com>
2017-04-09 10:56:00 -0400

    Update bsi-nginx.sh

--- bsi-nginx.sh
+++ bsi-nginx.sh
@@ -5,7 +5,7 @@ yum -y update
 sleep 2
 yum -y install screen
 sleep 2
-curl -O https://centminmod.com/betainstaller.sh
+wget -O /betainstaller.sh https://centminmod.com/betainstaller.sh
 chmod 0700 betainstaller.sh
 sleep 5
-/usr/bin/screen -d -m ~/betainstaller.sh
+/usr/bin/screen -d -m /betainstaller.sh
