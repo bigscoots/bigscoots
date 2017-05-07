125e48ff64674ab01beace7d3cb6e0fbfed0493d
Justin catello <justin@bigscoots.com>
2017-05-07 07:41:57 -0400

    Update ngxbackup.sh

--- ngxbackup.sh
+++ ngxbackup.sh
@@ -2,35 +2,40 @@
 # Usage: /bigscoots/ngxbackup.sh /home/nginx/domains/domain.com/public daily,weekly,monthly
 
 # Touch
-BKUSER=0000
-DOCROOT=$1
-FREQUENCY=$2
-RSERVER=int-backup2.bigscoots.com
+bkuser=b0000
+docroot=$1
+frequency=$2
+rserver=int-backup2.bigscoots.com
 
 # No Touch
-DB=$(grep DB_NAME "$DOCROOT"/wp-config.php | grep -Ev '//define|WP_CACHE_KEY_SALT' | sed 's/ //g' | sed -e "s/define('DB_NAME','//g" -e "s/');//g" | sed 's/\s.*$//' | tr -d '\r')
-SITE=$(echo "$DOCROOT" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
-CURRDATE=$(date +%Y-%m-%d)
-MYSQLDUMP=$(which mysqldump)
+#db=$(grep DB_NAME "$docroot"/wp-config.php | grep -Ev '//define|WP_CACHE_KEY_SALT' | sed 's/ //g' | sed -e "s/define('DB_NAME','//g" -e "s/');//g" | sed 's/\s.*$//' | tr -d '\r')
+db=$(grep DB_NAME "$docroot"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
+site=$(echo "$docroot" | sed -e "s/\/home\/nginx\/domains\///g" -e "s/\/public//g")
+currdate=$(date +%Y-%m-%d-%H%M)
+bfile=${site}_${currdate}
 
 # Remove old backups
-ssh "$BKUSER"@$RSERVER 'find ~/backup/$FREQUENCY/ -type d -ctime +10 -exec rm -rf {} \;'
+#ssh "$bkuser"@"$rserver" 'find ~/backup/$site/$frequency/ -type d -ctime +10 -exec rm -rf {} \;'
 
 # Create the backup directory on the backup server
-ssh "$BKUSER"@$RSERVER "mkdir -p ~/backup/$FREQUENCY/$CURRDATE"
+echo "Creating the backup directory for $site on the backup server: ~/backup/$site/$frequency"
+ssh "$bkuser"@"$rserver" "mkdir -p ~/backup/$site/$frequency"
 
 # Backup the database into the sites docroot
-$MYSQLDUMP "$DB" --single-transaction --quick --opt --skip-lock-tables --routines --triggers | gzip > "$DOCROOT"/"$DB".sql.gz
+echo "Taking a backup of the database $db for $site"
+mysqldump "$db" --single-transaction --quick --opt --skip-lock-tables --routines --triggers | gzip > "$docroot"/"$db".sql.gz
 
 # Tarbal the entire site including database directly onto the backup server
-# tar zvcf - "$DOCROOT" --exclude-from '/bigscoots/ngxbackup-excludes.txt' | ssh "$BKUSER"@$RSERVER "cat > ~/backup/$FREQUENCY/$CURRDATE/$SITE.tar.gz"
+echo "Now we are backing up the site files for $site"
+tar cfC - "$docroot" ./ --exclude-from=/bigscoots/ngxbackup-excludes.txt | pxz -9ez -T=0 - |  ssh "$bkuser"@"$rserver" "cat > ~/backup/$site/$frequency/$bfile.tar.xz"
 
 # Remove the database backup from the sites docroot
-rm -f "$DOCROOT"/"$DB".sql.gz
+echo "We are now removing he database backp from the source $docroot/$db.sql.gz"
+rm -f "$docroot"/"$db".sql.gz
 
 # rsync the uploads directory if it was excluded
-
-#ssh "$BKUSER"@$RSERVER "mkdir -p ~/backup/rsync/$SITE"
-#rsync -ahvz "$DOCROOT"/wp-content/uploads "$BKUSER"@"$RSERVER":~/backup/rsync/"$SITE"/
+echo "Running rsync for the $site"
+ssh "$bkuser"@"$rserver" "mkdir -p ~/backup/rsync/$site"
+rsync -ah "$docroot"/ "$bkuser"@"$rserver":~/backup/"$site"/rsync/
 
 # The end
