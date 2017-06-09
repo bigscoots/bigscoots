258d5a2c385189ea8fba751afe1ffb89ae0a1b9e
Justin catello <justin@bigscoots.com>
2017-06-09 11:16:07 -0400

    Create bsi-nginx.sh

--- bsi-nginx.sh
+++ bsi-nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sleep 30
+yum clean all
 yum -y update
 sleep 2
 yum -y install screen
@@ -9,3 +10,4 @@ cd /home
 curl -o betainstaller.sh -L https://centminmod.com/betainstaller.sh
 sleep 5
 /usr/bin/screen -d -m sh betainstaller.sh
+export EDITOR=nano
