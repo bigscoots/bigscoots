d9994f402553535e0966dcc17bf35251b553dc9d
Justin catello <justin@bigscoots.com>
2017-06-13 16:07:57 -0400

    Create bs_cpanel_initial_centos7.sh

---
+++ bs_cpanel_initial_centos7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum clean all
+
+if [[ ! -f /usr/bin/git || ! -f /usr/bin/curl || ! -f /usr/bin/nano ]]; then
+  echo
+  echo "installing yum packages..."
+  echo
+  yum -y install git nano wget curl
+fi
+  if [ -f /etc/selinux/config ]; then
+  sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config && setenforce 0
+  sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config && setenforce 0
+fi
+
+git clone https://github.com/jcatello/bigscoots
+/bigscoots/bs_cpanel_install_centos7.sh
