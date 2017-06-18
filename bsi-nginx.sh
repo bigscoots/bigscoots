a2bea04a034980837a3a31e79ea1f8d0cffa901b
Justin catello <justin@bigscoots.com>
2017-06-18 11:47:54 -0400

    Update bsi-nginx.sh

--- bsi-nginx.sh
+++ bsi-nginx.sh
@@ -4,11 +4,22 @@ sleep 30
 yum clean all
 yum -y update
 sleep 2
-yum -y install screen
-sleep 2
 cd /home
 curl -o betainstaller.sh -L https://centminmod.com/betainstaller.sh
 sleep 5
-/usr/bin/screen -d -m sh betainstaller.sh
+sh betainstaller.sh
 export EDITOR=nano
 timedatectl set-timezone America/Chicago
+wget -O /usr/local/src/centminmod/inc/wpsetup.inc https://raw.githubusercontent.com/jcatello/centminmod/master/inc/wpsetup.inc
+touch /etc/centminmod/email-primary.ini
+touch /etc/centminmod/email-secondary.ini
+echo "root" > /etc/centminmod/email-primary.ini
+echo "root" > /etc/centminmod/email-secondary.ini
+sed -i '/#root/c\root: /dev/null' /etc/aliases
+/bin/newaliases
+sleep 1
+rm -rf /usr/local/nginx/conf/conf.d/demodomain.com.conf /home/nginx/domains/demodomain.com
+sleep 1
+yum -y install redis --enablerepo=remi --disableplugin=priorities
+chkconfig redis on
+service redis start
