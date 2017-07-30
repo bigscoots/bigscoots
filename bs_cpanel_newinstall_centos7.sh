81ef0ea511af67d76fe65776af79554e0b8259d5
Justin catello <justin@bigscoots.com>
2017-07-30 08:03:22 -0400

    Create bs_cpanel_newinstall_centos7.sh

---
+++ bs_cpanel_newinstall_centos7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ping -c 1 google.com &> /dev/null
+then
+  crontab -r
+  yum clean all
+  curl -sL https://raw.githubusercontent.com/jcatello/bigscoots/master/bs_cpanel_initial_centos7.sh | bash
+  echo "cPanel install for $HOSTNAME completed" | mail -s "cPanel install for $HOSTNAME completed" monitor@bigscoots.com
+else
+  reboot
+fi
