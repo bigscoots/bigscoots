ad0e0e82bb4c636a73ca21b0fe2040f4be5929ff
Justin catello <justin@bigscoots.com>
2017-09-09 19:38:35 -0400

    Update bs_cpanel_newinstall_centos7.sh

--- bs_cpanel_newinstall_centos7.sh
+++ bs_cpanel_newinstall_centos7.sh
@@ -3,7 +3,7 @@ if ping -c 1 google.com &> /dev/null
 then
   crontab -r
   yum clean all
-  curl -sL https://raw.githubusercontent.com/jcatello/bigscoots/master/bs_cpanel_initial_centos7.sh | bash
+  curl -sL https://raw.githubusercontent.com/jcatello/bigscoots/master/bs_cpanel_initial_centos7.sh?1 | bash
   echo "cPanel install for $HOSTNAME completed" | mail -s "cPanel install for $HOSTNAME completed" monitor@bigscoots.com
 else
   reboot
