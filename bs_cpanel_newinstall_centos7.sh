faa38981aa7b48b453c58fe62333145608a109ba
Justin catello <justin@bigscoots.com>
2017-09-12 22:46:41 -0400

    Update bs_cpanel_newinstall_centos7.sh

--- bs_cpanel_newinstall_centos7.sh
+++ bs_cpanel_newinstall_centos7.sh
@@ -3,6 +3,10 @@ if ping -c 1 google.com &> /dev/null
 then
   crontab -r
   yum clean all
+  yum -y install e2fsprogs
+  hostnamectl set-hostname $(grep -v '#\|local' /etc/hosts| awk '{print $2}')
+  chattr +i /etc/hostname
+  bash
   curl -sL https://raw.githubusercontent.com/jcatello/bigscoots/master/bs_cpanel_initial_centos7.sh?1 | bash
   echo "cPanel install for $HOSTNAME completed" | mail -s "cPanel install for $HOSTNAME completed" monitor@bigscoots.com
 else
