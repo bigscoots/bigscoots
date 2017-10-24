8373b8a3d317390e0b329e7166fdf37dfdf755db
Justin catello <justin@bigscoots.com>
2017-10-24 09:19:21 -0400

    Update bs_cpanel_install_centos7.sh

--- bs_cpanel_install_centos7.sh
+++ bs_cpanel_install_centos7.sh
@@ -278,7 +278,7 @@ whmapi1 set_tweaksetting key=smtpmailgidonly value=0
 wget -O /usr/local/sbin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar ; chmod +x /usr/local/sbin/wp
 ln -s /usr/local/sbin/wp /usr/sbin/wp
 echo "alias wp='/opt/cpanel/ea-php70/root/usr/bin/php /usr/local/sbin/wp --allow-root'" >> /root/.bashrc
-crontab -l | { cat; echo "* * * * * /bigscoots/chkphpfpm_nginx"; } | crontab -
+crontab -l | { cat; echo "* * * * * /bigscoots/chkphpfpm"; } | crontab -
 crontab -l | { cat; echo "*/15 * * * * /bigscoots/mon_disk.sh"; } | crontab -
 sed -i 's/export PATH/export PATH\nexport EDITOR=nano/g' /root/.bash_profile
 
