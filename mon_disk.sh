7110bbeb1e16fc7200df97945d98468c350ee8c3
Justin catello <justin@bigscoots.com>
2017-11-03 00:46:24 -0400

    Update mon_disk.sh

--- mon_disk.sh
+++ mon_disk.sh
@@ -4,7 +4,18 @@ do
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo "$output" | awk '{ print $2 }' )
   if [ "$usep" -ge 95 ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
-     mail -s "Alert: Almost out of disk space $usep% on $HOSTNAME" monitor@bigscoots.com
+    (
+        echo "Hello,"
+        echo "Your server is currently using ($usep%) of its available disk space."
+        echo "Please try and free up space or see about upgrading to the next package to increase your available disk space."
+        echo "You can also reply back to this email to open a support ticket to request assistance."
+        echo ""
+        echo "Regards"
+        echo "The Bigscoots Team"
+        echo ""
+        echo ""
+        echo "Automated Response $(date)"
+
+     ) | mail -s "BigScoots Alert: Almost out of disk space $usep% -  $(hostname)" -S replyto="support@bigscoots.com" monitor@bigscoots.com
   fi
 done
