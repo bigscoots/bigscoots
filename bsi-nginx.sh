a1c5daa3bdfa4523ae78c1161f4adea310c2d902
Justin catello <justin@bigscoots.com>
2018-01-09 17:45:14 -0500

    Update bsi-nginx.sh

--- bsi-nginx.sh
+++ bsi-nginx.sh
@@ -9,7 +9,15 @@ yum -y update
 sleep 2
 cd /home
 curl -o betainstaller.sh -L https://centminmod.com/betainstaller.sh
+
 sleep 5
+
+PHPVER=$(curl -s http://php.net/downloads.php |grep -o "php-7.1.[0-9][0-9].tar.gz" | sed 's/php-//g; s/.tar.gz//g' | uniq)
+PHPVER_REPLACE=$(grep PHP_VERSION betainstaller.sh | sed 's/# //g' | sed "s/PHP_VERSION='[0-9].*'/PHP_VERSION='$PHPVER'/g")
+sed -i '/PHP_VERSION/c\'"$PHPVER_REPLACE" betainstaller.sh |grep PHP_VERSION
+
+sleep 3
+
 sh betainstaller.sh
 export EDITOR=nano
 timedatectl set-timezone America/Chicago
