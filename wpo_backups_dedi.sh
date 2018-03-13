3ff7b27a8fc38b54738d98a7a6cc200def60ea67
Justin catello <justin@bigscoots.com>
2018-03-12 21:15:21 -0400

    Update wpo_backups_dedi.sh

--- wpo_backups_dedi.sh
+++ wpo_backups_dedi.sh
@@ -2,16 +2,13 @@
 
 date=$(date "+%Y-%m-%dT%H_%M_%S")
 HOME=/home/nginx/domains/
-BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
-BKSVR=int-backup3.bigscoots.com
 
 rsync -azP \
   --delete \
   --delete-excluded \
   --exclude-from="$HOME".rsync/exclude \
   --link-dest=../current \
-  "$HOME" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
-  && ssh -i ~/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
-  "mv incomplete_back-$date back-$date \
-  && rm -f current \
-  && ln -s back-$date current"
+  "$HOME" /backup/incomplete_back-"$date" \
+  mv /backup/incomplete_back-"$date" back-"$date" \
+  && rm -f /backup/current \
+  && ln -s /backup/back-"$date" /backup/current
