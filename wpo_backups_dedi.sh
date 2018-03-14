81b60a87d311e3b3894871b488b5e16b9bfc46d3
Justin catello <justin@bigscoots.com>
2018-03-14 18:13:06 -0400

    Update wpo_backups_dedi.sh

--- wpo_backups_dedi.sh
+++ wpo_backups_dedi.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 date=$(date "+%Y-%m-%dT%H_%M_%S")
-HOME=/home/nginx/domains/
+HOMEDIR=/home/nginx/domains/
 
-mkdir /home/nginx/domains/.rsync
+if [ ! -f "$HOMEDIR".rsync/exclude ]; then
+	mkdir -p "$HOMEDIR".rsync
 
-{
-echo "log"
-echo "*/core.[0-9]*"
-echo "*/error_log"
-echo "*/wp-content/updraft"
+	{
+	echo "log"
+	echo "*/core.[0-9]*"
+	echo "*/error_log"
+	echo "*/wp-content/updraft"
+	echo "*/wp-content/cache"
+	
+	} > "$HOMEDIR".rsync/exclude
+else
+	:
+fi
 
-} > /home/nginx/domains/.rsync/exclude
-
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
+for wpinstall in $(find "$HOMEDIR"*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
     dbname=$(grep DB_NAME "$wpinstall"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
     /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
@@ -22,18 +27,17 @@ done
 rsync -azP \
   --delete \
   --delete-excluded \
-  --exclude-from="$HOME"/.rsync/exclude \
+  --exclude-from="$HOMEDIR"/.rsync/exclude \
   --link-dest=../current \
-  "$HOME" /backup/incomplete_back-"$date" \
-  mv /backup/incomplete_back-"$date" back-"$date" \
+  "$HOMEDIR" /backup/incomplete_back-"$date" \
+  mv /backup/incomplete_back-"$date" /backup/back-"$date" \
   && rm -f /backup/current \
   && ln -s /backup/back-"$date" /backup/current
   
   #!/bin/bash
 
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
+for wpinstall in $(find "$HOMEDIR"*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
     dbname=$(grep DB_NAME "$wpinstall"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
     rm -f "$wpinstall$dbname".sql.gz
 done
-
