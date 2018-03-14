07c0743e7585c8a7dee6ac4b90e956fbb33e2cfd
Justin catello <justin@bigscoots.com>
2018-03-14 15:46:53 -0400

    Update wpo_backups_ovz.sh

--- wpo_backups_ovz.sh
+++ wpo_backups_ovz.sh
@@ -1,15 +1,29 @@
+#!/bin/bash
+
 date=$(date "+%Y-%m-%dT%H_%M_%S")
-HOME=/home/nginx/domains/
+HOMEDIR=/home/nginx/domains/
 BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
 BKSVR=int-backup3.bigscoots.com
+
+for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
+   do
+    dbname=$(grep DB_NAME "$wpinstall"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
+    /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
+done
 
 rsync -azP \
   --delete \
   --delete-excluded \
-  --exclude-from="$HOME".rsync/exclude \
+  --exclude-from="$HOMEDIR".rsync/exclude \
   --link-dest=../current \
-  "$HOME" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
-  && ssh -i ~/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
+  "$HOMEDIR" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
+  && ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
   "mv incomplete_back-$date back-$date \
   && rm -f current \
   && ln -s back-$date current"
+
+for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
+   do
+    dbname=$(grep DB_NAME "$wpinstall"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
+    rm -f "$wpinstall$dbname".sql.gz
+done
