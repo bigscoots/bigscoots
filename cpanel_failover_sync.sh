246b816d624cdf60d2fbd5aadfd77331456944a0
Justin catello <justin@bigscoots.com>
2018-03-16 17:22:18 -0400

    Create cpanel_failover_sync.sh

---
+++ cpanel_failover_sync.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+liveserver=192.168.1.196
+
+ rm -f /tmp/remoteusers.txt /tmp/localusers.txt 
+ ssh -p 2222 "$liveserver" "whmapi1 listaccts" |grep user: |awk '{print $2}' >> /tmp/remoteusers.txt 
+ whmapi1 listaccts |grep user: |awk '{print $2}' >> /tmp/localusers.txt 
+ for i in $(grep -Fxv -f /tmp/remoteusers.txt /tmp/localusers.txt) 
+ do 
+ /scripts/removeacct --force "$i"
+  done
+
+rm -f /tmp/remoteusers.txt /tmp/localusers.txt 
+ssh -p 2222 "$liveserver" "whmapi1 listaccts" |grep user: |awk '{print $2}' >> /tmp/remoteusers.txt
+whmapi1 listaccts |grep user: |awk '{print $2}' >> /tmp/localusers.txt 
+
+xferid=$(whmapi1 create_remote_root_transfer_session remote_server_type=cpanel host="$liveserver" port=2222 user=root sshkey_name=id_rsa transfer_threads=10 restore_threads=10 unrestricted_restore=1 copy_reseller_privs=0 compressed=0 unencrypted=0 low_priority=0 |grep transfer_session_id: | awk '{print $2}')
+ 
+for cpuser in $(grep -Fxv -f /tmp/localusers.txt /tmp/remoteusers.txt) 
+do 
+	whmapi1 enqueue_transfer_item transfer_session_id="$xferid" module=AccountRemoteRoot user="$cpuser" localuser="$cpuser" force=1 overwrite_sameowner_dbs=1 overwrite_sameowner_dbusers=1 skiphomedir=1 skipbwdata=1
+done
+
+for cpuser in $(ssh -p 2222 "$liveserver" "whmapi1 listaccts" |grep user: |awk '{print $2}') ; do 
+	whmapi1 enqueue_transfer_item transfer_session_id="$xferid" module=AccountRemoteRoot user="$cpuser" localuser="$cpuser" force=1 overwrite_sameowner_dbs=1 overwrite_sameowner_dbusers=1 skiphomedir=1 skipbwdata=1 skipaccount=1
+
+done
+
+	whmapi1 start_transfer_session transfer_session_id="$xferid"
