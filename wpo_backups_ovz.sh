04ed829da482cd0182087482cede5a3786ff0155
Justin catello <justin@bigscoots.com>
2018-03-17 18:17:22 -0400

    Update wpo_backups_ovz.sh

--- wpo_backups_ovz.sh
+++ wpo_backups_ovz.sh
@@ -5,6 +5,27 @@ HOMEDIR=/home/nginx/domains/
 BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
 BKSVR=int-backup3.bigscoots.com
 
+if ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime'; [ $? -eq 255 ]
+then
+  echo "WPO failed to SSH to backup server." | mail -s "$HOSTNAME- WPO failed to SSH to backup server." monitor@bigscoots.com
+  exit 1
+fi
+
+if [ ! -f "$HOMEDIR".rsync/exclude ]; then
+        mkdir -p "$HOMEDIR".rsync
+
+        {
+        echo "log"
+        echo "*/core.[0-9]*"
+        echo "*/error_log"
+        echo "*/wp-content/updraft"
+        echo "*/wp-content/cache"
+
+        } > "$HOMEDIR".rsync/exclude
+else
+        :
+fi
+
 for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
     dbname=$(grep DB_NAME "$wpinstall"/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
@@ -12,6 +33,7 @@ for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.ph
 done
 
 rsync -azP \
+  -e "ssh -i $HOME/.ssh/wpo_backups" \
   --delete \
   --delete-excluded \
   --exclude-from="$HOMEDIR".rsync/exclude \
