c3ab9706ce7e91f3ec8790b6e95bb82616e9125e
root <root@wpo.theessentialfamily.com>
2018-05-26 10:30:11 -0500

    added cronjob for redischk

--- bsi-nginx.sh
+++ bsi-nginx.sh
@@ -48,6 +48,7 @@ git clone https://github.com/jcatello/bigscoots
 chown -R nginx: /var/log/php-fpm
 nprestart
 crontab -l | { cat; echo "* * * * * /bigscoots/chkphpfpm_nginx"; } | crontab -
+crontab -l | { cat; echo "* * * * * /bigscoots/redischk.sh"; } | crontab -
 crontab -l | { cat; echo "*/15 * * * * /bigscoots/mon_disk.sh"; } | crontab -
 crontab -l | { cat; echo "$(( ( RANDOM % 60 )  + 1 )) $(( ( RANDOM % 4 )  + 1 )) * * * /bigscoots/wpo_backups_ovz.sh"; } | crontab -
 mkdir ~/.ssh
