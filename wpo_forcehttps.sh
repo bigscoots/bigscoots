f6e4165c4f8f3eeac2c72853dc1afce350071510
root <root@joxier.bigscoots-staging.com>
2018-08-23 12:34:26 -0500

    added wpo_forcehttps.sh

---
+++ wpo_forcehttps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+
+then
+
+for i in $(find /usr/local/nginx/conf/conf.d/ -type f -printf "%f\n" |grep -v 'phpmyadmin_ssl.conf\|virtual.conf\|.ssl.conf') ; do 
+
+domain="${i//.conf/}"
+
+cp /usr/local/nginx/conf/conf.d/"$domain".conf{,.bak}
+
+{
+echo "  server {"
+echo "            listen   80;"
+echo "            server_name $domain www.$domain;"
+echo "            return 301 https://$domain\$request_uri;"
+echo "       }"
+
+
+} > /usr/local/nginx/conf/conf.d/"$domain.conf"
+
+done
+
+else
+
+domain="${1//.conf/}"
+
+cp /usr/local/nginx/conf/conf.d/"$domain".conf{,.bak}
+
+{
+echo "  server {"
+echo "            listen   80;"
+echo "            server_name $domain www.$domain;"
+echo "            return 301 https://$domain\$request_uri;"
+echo "       }"
+
+
+} > /usr/local/nginx/conf/conf.d/"$domain.conf"
+
+fi
