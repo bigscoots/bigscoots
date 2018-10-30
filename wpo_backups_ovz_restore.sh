9f9132d374a97d8f7b4003a25477b348fc4438a5
Justin catello <justin@bigscoots.com>
2018-10-30 00:49:12 -0400

    Update wpo_backups_ovz_restore.sh

--- wpo_backups_ovz_restore.sh
+++ wpo_backups_ovz_restore.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# options
+# h = human readable
 
 BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
-BKSVR=int-backup3.bigscoots.com
+BKSVR=$(grep BKSVR= /bigscoots/wpo_backups_ovz.sh | sed 's/BKSVR=//g')
 
 if ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime'; [ $? -eq 255 ]
 then
@@ -11,6 +13,19 @@ fi
 
 echo "Available Backups:"
 
+if [[ $1 = h ]]
+
+then
+
+ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'ls -1 | sed "s/incomplete_back-/Incomplete Backup: /g ; s/back-/Full Backup: /g ; s/T/ /g ; s/_/:/g"';
+
+else
+
 ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'ls -1';
 
-echo "rsync -ahv -e ssh -i $HOME/.ssh/wpo_backups --delete $BKUSER@$BKSVR:~/current/"
+fi
+
+echo
+echo "Example rsync command - You should run a backup before proceeding"
+echo "# rsync -ahv -e ssh -i $HOME/.ssh/wpo_backups --delete $BKUSER@$BKSVR:~/current/"
+echo
