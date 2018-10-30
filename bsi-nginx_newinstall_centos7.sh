9a11566006d7964d875796fa19e743311461b9a6
Justin catello <justin@bigscoots.com>
2018-10-29 23:57:34 -0400

    Update bsi-nginx_newinstall_centos7.sh

--- bsi-nginx_newinstall_centos7.sh
+++ bsi-nginx_newinstall_centos7.sh
@@ -4,7 +4,6 @@ then
   crontab -r
   yum clean all
   curl -sL https://raw.githubusercontent.com/jcatello/bigscoots/master/bsi-nginx.sh | bash
-  echo "nginx install for $HOSTNAME completed" | mail -s "nginx install for $HOSTNAME completed" monitor@bigscoots.com
 else
   reboot
 fi
