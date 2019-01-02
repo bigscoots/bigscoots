e9b1def9860413891607225c31886fd4f39bcbd5
Justin catello <justin@bigscoots.com>
2019-01-02 14:27:07 -0500

    Delete detect3.sh

--- detect3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-find . -name '*.php' | while read fname
-do
-tail -n 1 $fname |grep -E '[0-9a-fA-F]{6}' > ~/tmp/virusdetect.out
-if [[ -s ~/tmp/virusdetect.out ]]
-then
-        echo "$fname: VIRUS FOUND!"
-fi
-done
