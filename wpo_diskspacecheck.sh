61f2958ecf50b289eac9ee6f49ebc9978b822243
Justin catello <justin@bigscoots.com>
2019-02-08 12:21:01 -0500

    Update wpo_diskspacecheck.sh

--- wpo_diskspacecheck.sh
+++ wpo_diskspacecheck.sh
@@ -1,68 +1,27 @@
 #!/bin/bash
 
-RE='^[0-9]+$'
-
 if [[ $# -eq 0 ]]; then
     echo >&2 "Requires arguments:"
-    echo >&2 "CTID"
+    echo >&2 "domain.com"
     echo >&2 "ALL"
     exit 1
 fi
 
 if [[ $1 = ALL ]] ; then
 
-        vzlist \
-        | grep -v HOSTNAME \
-        | grep wpo \
-        | awk '{print $1, $5}' \
-        |  while read -r id hostname
-
-          do
-
-          CTID="$id"
-          VZHOSTNAME="$hostname"
-          FDU=$(du -sh /vz/root/"$CTID"/home/nginx/domains/ | awk '{print $1}')
-          DDU=$(du -sh --exclude 'ib_logfile*' --exclude 'ibdata*' /vz/root/"$CTID"/var/lib/mysql/ | awk '{print $1}')
-          TDU=$(du -sc --exclude 'ib_logfile*' --exclude 'ibdata*' /vz/root/"$CTID"/var/lib/mysql/ /vz/root/"$CTID"/home/nginx/domains/ | tail -1 | awk '{print $1}' | awk '{ byte =$1 /1024/1024; print byte " GB" }')
-
+          FDU=$(du -sh /home/nginx/domains/ | awk '{print $1}')
+          DDU=$(du -sh --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ | awk '{print $1}')
+          TDU=$(du -sc --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ /home/nginx/domains/ | tail -1 | awk '{print $1}'| awk '{ split( "KB MB GB" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }')
           echo "File Disk Usage: $FDU"
           echo "Database Disk Usage: $DDU"
           echo "Total Disk Usage: $TDU"
 
-  if ((${TDU%.*} > 10 )) ; then
-        du -sh /vz/root/"$CTID"/home/nginx/domains/*/public/*/*/*/* \
-        | sort -h \
-        | tail -10 \
-        | sed "s=/vz/root/$CTID/home/nginx/domains/==g" \
-        | mail -s "WPO over 10GB - $VZHOSTNAME - $TDU " monitor@bigscoots.com
-  fi
+elif [ -n "$1" ] ; then
 
-          done
-
-elif [[ $1 =~ $RE ]] ; then
-
-CTID=$1
-VZHOSTNAME=$(vzlist | grep "$CTID" | awk '{print $5}')
-FDU=$(du -sh /vz/root/"$CTID"/home/nginx/domains/ | awk '{print $1}')
-DDU=$(du -sh --exclude 'ib_logfile*' --exclude 'ibdata*' /vz/root/"$CTID"/var/lib/mysql/ | awk '{print $1}')
-TDU=$(du -sc --exclude 'ib_logfile*' --exclude 'ibdata*' /vz/root/"$CTID"/var/lib/mysql/ /vz/root/"$CTID"/home/nginx/domains/ | tail -1 | awk '{print $1}' | awk '{ byte =$1 /1024/1024; print byte " GB" }')
-
-echo "File Disk Usage: $FDU"
-echo "Database Disk Usage: $DDU"
-echo "Total Disk Usage: $TDU"
-
-  if ((${TDU%.*} > 10 )) ; then
-        du -sh /vz/root/"$CTID"/home/nginx/domains/*/public/*/*/*/* \
-        | sort -h \
-        | tail -10 \
-        | sed "s=/vz/root/$CTID/home/nginx/domains/==g" \
-        | mail -s "WPO over 10GB - $VZHOSTNAME - $TDU " monitor@bigscoots.com
+         FDU="$(du -sh /home/nginx/domains/"$1" | awk '{print $1}')"
+         DDU="$(du -sh /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" | awk '{print $1}')"
+         TDU="$(du -sc /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" /home/nginx/domains/"$1" | tail -1 | awk '{print $1}'| awk '{ split( "KB MB GB" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }')"
+         echo "File Disk Usage: $FDU"
+         echo "Database Disk Usage: $DDU"
+         echo "Total Disk Usage: $TDU"
 fi
-else
-
-    echo >&2 "Requires arguments:"
-    echo >&2 "CTID"
-    echo >&2 "ALL"
-    exit 1
-
-  fi
