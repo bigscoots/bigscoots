dfc97e9a8e7a1fbe3ca6d8c60ae358877c1959a6
Justin catello <justin@bigscoots.com>
2019-02-12 14:02:17 -0500

    Update wpo_diskspacecheck.sh

--- wpo_diskspacecheck.sh
+++ wpo_diskspacecheck.sh
@@ -9,19 +9,19 @@ fi
 
 if [[ $1 = ALL ]] ; then
 
-          FDU=$(du -sh /home/nginx/domains/ | awk '{print $1}')
-          DDU=$(du -sh --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ | awk '{print $1}')
-          TDU=$(du -sc --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ /home/nginx/domains/ | tail -1 | awk '{print $1}'| awk '{ split( "KB MB GB" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }')
-          echo "File Disk Usage: $FDU"
-          echo "Database Disk Usage: $DDU"
-          echo "Total Disk Usage: $TDU"
+
+    while read size unit ; do fdusize=$size ; fduunit=$unit ; done <<< $(du -sh /home/nginx/domains/ | awk '{print $1}' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+    while read size unit ; do ddusize=$size ; dduunit=$unit ; done <<< $(du -sh --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ | awk '{print $1}' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+    while read size unit ; do tdusize=$size ; tduunit=$unit ; done <<< $(du -sc --exclude 'ib_logfile*' --exclude 'ibdata*' /var/lib/mysql/ /home/nginx/domains/ | tail -1 | awk '{print $1}'| awk '{ split( "K M G" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+
+    echo "[{\"name\": \"File Disk Usage\",\"size\": \"$fdusize\",\"value\": \"$fduunit\"},{\"name\": \"Database Disk Usage\",\"size\": \"$ddusize\",\"value\": \"$dduunit\"},{\"name\": \"Total Disk Usage\",\"size\": \"$tdusize\",\"value\": \"$tduunit\"}]"
 
 elif [ -n "$1" ] ; then
 
-         FDU="$(du -sh /home/nginx/domains/"$1" | awk '{print $1}')"
-         DDU="$(du -sh /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" | awk '{print $1}')"
-         TDU="$(du -sc /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" /home/nginx/domains/"$1" | tail -1 | awk '{print $1}'| awk '{ split( "KB MB GB" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }')"
-         echo "File Disk Usage: $FDU"
-         echo "Database Disk Usage: $DDU"
-         echo "Total Disk Usage: $TDU"
+    while read size unit ; do fdusize=$size ; fduunit=$unit ; done <<< $(du -sh /home/nginx/domains/"$1" | awk '{print $1}' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+    while read size unit ; do ddusize=$size ; dduunit=$unit ; done <<< $(du -sh /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" | awk '{print $1}' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+    while read size unit ; do tdusize=$size ; tduunit=$unit ; done <<< $(du -sc /var/lib/mysql/"$(grep DB_NAME /home/nginx/domains/"$1"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | grep -v '^//' | cut -d \' -f 4)" /home/nginx/domains/"$1" | tail -1 | awk '{print $1}'| awk '{ split( "K M G" , v ); s=1; while( $1>1024 ){ $1/=1024; s++ } print int($1) v[s] }' | grep -Eo '[[:alpha:]]+|[0-9.?]+')
+
+    echo "[{\"name\": \"File Disk Usage\",\"size\": \"$fdusize\",\"value\": \"$fduunit\"},{\"name\": \"Database Disk Usage\",\"size\": \"$ddusize\",\"value\": \"$dduunit\"},{\"name\": \"Total Disk Usage\",\"size\": \"$tdusize\",\"value\": \"$tduunit\"}]"
+
 fi
