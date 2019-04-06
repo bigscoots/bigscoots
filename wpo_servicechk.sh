2c7cbc87c4c6dc38b387076e97f7fab0d80adfd3
Justin catello <justin@bigscoots.com>
2019-04-06 12:44:05 -0400

    Update wpo_servicechk.sh

--- wpo_servicechk.sh
+++ wpo_servicechk.sh
@@ -5,7 +5,7 @@ if [ $? != 0 ]
 then
        /etc/init.d/nginx start > /dev/null
 fi
-ps -ef | grep php5-fpm |grep -v grep > /dev/null
+ps -ef | grep php-fpm |grep -v grep > /dev/null
 if [ $? != 0 ]
 then
        /etc/init.d/php-fpm start > /dev/null
