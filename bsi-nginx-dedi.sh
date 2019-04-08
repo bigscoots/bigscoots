c63b1579d0d449930e71e84b02d9392029da970d
Justin catello <justin@bigscoots.com>
2019-04-07 20:17:00 -0400

    Update bsi-nginx-dedi.sh

--- bsi-nginx-dedi.sh
+++ bsi-nginx-dedi.sh
@@ -152,6 +152,7 @@ service postfix restart
 
 crontab -l | { cat; echo "* * * * * /bigscoots/chkphpfpm_nginx"; } | crontab -
 crontab -l | { cat; echo "*/15 * * * * /bigscoots/mon_disk.sh"; } | crontab -
+crontab -l | { cat; echo "* * * * * /bigscoots/wpo_servicechk.sh"; } | crontab -
 crontab -l | { cat; echo "0 */6 * * * /usr/bin/cmupdate 2>/dev/null ; /bigscoots/wpo_update.sh 2>/dev/null ; wget -O /usr/local/src/centminmod/inc/wpsetup.inc https://raw.githubusercontent.com/jcatello/centminmod/master/inc/wpsetup.inc"; } | crontab -
 crontab -l | sed 's/.*autoprotect/#&/' | crontab -
 
