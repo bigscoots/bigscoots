fabf3b4046b8243c0a962f45dea0ffcfb2defcd9
Justin catello <justin@bigscoots.com>
2019-04-16 19:33:46 -0400

    Update wpo_redirect.sh

--- wpo_redirect.sh
+++ wpo_redirect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# domain.com domain.com predef add rule1,2,3,4,5,etc code
-# domain.com domain.com manual add source target code
+# domain.com domain.com predef add rule1,2,3,4,5,etc 301,302
+# domain.com domain.com manual add source target 301,302
 
 # Redirects
 
@@ -23,11 +23,13 @@ if [[ $2 == manual ]]; then
 	if ! grep -q wpo_manual_redirects.conf /usr/local/nginx/conf/conf.d/"$domain".ssl.conf; then
 
 	sed -i "/location \/ {/a \  include \/usr\/local\/nginx\/conf\/wpincludes\/$domain\/wpo_manual_redirects.conf;" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
+
 	fi
 
 	if ! grep -q wpo_manual_redirects.conf /usr/local/nginx/conf/conf.d/"$domain".conf; then
 
 	sed -i "/location \/ {/a \            include \/usr\/local\/nginx\/conf\/wpincludes\/$domain\/wpo_manual_redirects.conf;" /usr/local/nginx/conf/conf.d/"$domain".conf
+
 	fi
 
 	touch "/usr/local/nginx/conf/wpincludes/$domain/wpo_manual_redirects.conf"
@@ -38,16 +40,24 @@ if [[ $2 == manual ]]; then
 
 		uuid=$(uuidgen -r | sed 's/-//g')
 		n=0
-   		  until [ $n -ge 10 ]
-   		  do
+   		until [ $n -ge 10 ]
+   		do
       		grep -q "$uuid" /usr/local/nginx/conf/wpincludes/"$domain"/wpo_manual_redirects.conf && break
-     	 	n=$((n+1))
-      	  done
+     		n=$((n+1))
+      	done
 
 		source="$4"
 		target="$5"
 		code="$6"
-		echo "rewrite ^/$source/?$ $target $code # $uuid" >> "/usr/local/nginx/conf/wpincludes/$domain/wpo_manual_redirects.conf"
+
+		if [[ $code == 301 ]]; then
+			ngxcode="permanent\;"
+		elif [[ $code == 302 ]]; then
+			ngxcode="redirect\;"
+		fi
+
+		echo "rewrite ^/$source/?$ $target $ngxcode # $uuid" >> "/usr/local/nginx/conf/wpincludes/$domain/wpo_manual_redirects.conf"
+
 	fi
 
 fi
