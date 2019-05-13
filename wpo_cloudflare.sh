61af4a702230a66be69ee96e185906640659e5d8
Justin catello <justin@bigscoots.com>
2019-05-13 08:56:48 -0400

    Update wpo_cloudflare.sh

--- wpo_cloudflare.sh
+++ wpo_cloudflare.sh
@@ -32,6 +32,18 @@ if [[ ! -s /root/.wpocf ]]; then
 
   echo "\"cloudflare_username\": \"$cfemail\"," >> /root/.wpocf  ; echo "\"cloudflare_password\": \"$cfpass\"," >> /root/.wpocf  ; echo "\"cloudflare_userkey\": \"$cfuserkey\"," >> /root/.wpocf  ; echo "\"cloudflare_apikey\": \"$cfapikey\"," >> /root/.wpocf  ; read -r ns1 ns2 <<<$(echo $nameservers) >> /root/.wpocf  ; echo "\"cloudflare_nameserver_1\": \"$ns1\"," >> /root/.wpocf  ; echo "\"cloudflare_nameserver_2\": \"$(echo $ns2 | awk '{print $1}')\"" >> /root/.wpocf ; echo ; cat /root/.wpocf
   fi
+
 else
-echo hi
+
+  if ! grep -q NA.ns.cloudflare.com /root/.wpocf ; then
+    cfuserkey=$(grep cloudflare_userkey /root/.wpocf | sed 's/"//g; s/,//g' | awk '{print $2}')
+
+    nameservers=$(for i in $(\ls /home/nginx/domains/)  ; do
+    curl -s https://api.cloudflare.com/host-gw.html \
+    -d 'act=full_zone_set' \
+    -d 'host_key=e3471ba7eea3a19d1459332492e51679' \
+    -d "user_key=$cfuserkey" \
+    -d "zone_name=$i"
+    done | grep -oh "\w*.ns.cloudflare.com\w*")
+  fi
 fi
