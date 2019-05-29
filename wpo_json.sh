31684967d6f73afa38d7394cce36754a3245be50
Justin catello <justin@bigscoots.com>
2019-05-28 22:24:19 -0400

    Update wpo_json.sh

--- wpo_json.sh
+++ wpo_json.sh
@@ -24,8 +24,8 @@ for domain in $(\ls /home/nginx/domains/) ; do
 pmaurl=$(grep "https://$HOSTNAME/[0-9]" /root/centminlogs/centminmod_phpmyadmin_install_*.log | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" | sed 's/$/\//g' | tr -d " \t\n\r")
 pmauser=$(grep Username: /root/centminlogs/centminmod_phpmyadmin_install_*.log | awk '{print $2}')
 pmapass=$(grep Password: /root/centminlogs/centminmod_phpmyadmin_install_*.log | awk '{print $2}')
-pmadbuser=$(grep DB_USER /home/nginx/domains/"$domain"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
-pmadbpass=$(grep DB_PASSWORD /home/nginx/domains/"$domain"/public/wp-config.php | grep -v WP_CACHE_KEY_SALT | cut -d \' -f 4)
+pmadbuser=$(wp --allow-root config get DB_USER --path=/home/nginx/domains/"$domain"/public/)
+pmadbpass=$(wp --allow-root config get DB_PASSWORD --path=/home/nginx/domains/"$domain"/public/)
 
 vhostlog=$(grep -rl "FTP username created for $domain" /root/centminlogs/*wordpress_addvhost.log)
 grep -v "FTP Passive" "$vhostlog" | grep -C2 "FTP mode" > /tmp/tmpftp.txt
