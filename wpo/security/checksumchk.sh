a17dbec656700bcd5cbdb8a3d7a2b089be25a56c
Justin catello <justin@bigscoots.com>
2019-06-20 22:36:39 -0400

    Update checksumchk.sh

--- wpo/security/checksumchk.sh
+++ wpo/security/checksumchk.sh
@@ -8,7 +8,7 @@ for wpinstall in /home/nginx/domains/*/public ; do
 checksumexitcode=$?
 
 if [ $checksumexitcode -ne 0 ]; then
-    /bin/wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Core Checksum failed: $wpinstall" support@blueecho.ca
+    /bin/wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Core Checksum failed: $wpinstall" monitor@bigscoots.com
 fi
 
 /bin/wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" >/dev/null 2>&1
@@ -16,7 +16,7 @@ fi
 checksumexitcode=$?
 
 if [ $checksumexitcode -ne 0 ]; then
-   /bin/wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Plugins Checksum failed: $wpinstall" support@blueecho.ca
+   /bin/wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Plugins Checksum failed: $wpinstall" monitor@bigscoots.com
 fi
 
 done
