6b804558699ac74af6da4bdd044ffd69e285cc67
Justin catello <justin@bigscoots.com>
2019-06-21 10:50:24 -0400

    Update checksumchk.sh

--- wpo/security/checksumchk.sh
+++ wpo/security/checksumchk.sh
@@ -3,20 +3,21 @@
 
 for wpinstall in /home/nginx/domains/*/public ; do
 
-/bin/wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" >/dev/null 2>&1
+wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" >/dev/null 2>&1
 
 checksumexitcode=$?
+domain=$(echo $wpinstall | sed 's/\/home\/nginx\/domains\///g' | sed 's/\/public//g')
 
 if [ $checksumexitcode -ne 0 ]; then
-    /bin/wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Core Checksum failed: $wpinstall" monitor@bigscoots.com
+    wp core verify-checksums --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Core Checksum failed: $domain" monitor@bigscoots.com
 fi
 
-/bin/wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" >/dev/null 2>&1
+wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" >/dev/null 2>&1
 
 checksumexitcode=$?
 
 if [ $checksumexitcode -ne 0 ]; then
-   /bin/wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Plugins Checksum failed: $wpinstall" monitor@bigscoots.com
+   wp plugin verify-checksums --all --allow-root --skip-plugins --skip-themes --path="$wpinstall" 2>&1 | mail -s "WordPress Plugins Checksum failed: $domain" monitor@bigscoots.com
 fi
 
 done
