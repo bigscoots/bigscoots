4eecad52d9be7f3b3b5fca2e5a98aa830a617853
Justin catello <justin@bigscoots.com>
2019-08-06 15:56:50 -0400

    Update installer.sh

--- directadmin/installer.sh
+++ directadmin/installer.sh
@@ -17,7 +17,7 @@ IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\
 ETH=$(ip addr |grep ${IP} | grep -o '[^ ]\+$')
 read -p "Enter DA LID: " LID
 
-./setup.sh 31392 ${LID} ${HOSTNAME} venet0:0 ${IP}
+./setup.sh 31392 ${LID} ${HOSTNAME} ${ETH} ${IP}
 
 # Upgrade mysql to mariadb10.3
 
