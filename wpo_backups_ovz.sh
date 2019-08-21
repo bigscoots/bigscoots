8edee2ba63e1a8b65083a379e1fe3e54ced59189
Justin catello <justin@bigscoots.com>
2019-08-20 23:04:23 -0400

    Update wpo_backups_ovz.sh

--- wpo_backups_ovz.sh
+++ wpo_backups_ovz.sh
@@ -5,82 +5,125 @@ HOMEDIR=/home/nginx/domains/
 BKSVR=backup3.bigscoots.com
 BSPATH=/root/.bigscoots
 PATH=/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/root/bin
-
-if [ -f /proc/vz/veinfo ];
-then
-
-BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
-
-if ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime' >/dev/null; [ $? -eq 255 ]
-then
-  echo "Mark for Justin" | mail -s "$HOSTNAME- WPO failed to SSH to backup server." monitor@bigscoots.com
-  exit 1
-fi
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes"
 
 if [ ! -f "$BSPATH"/rsync/exclude ]; then
-        mkdir -p "$BSPATH"/rsync
+  mkdir -p "$BSPATH"/rsync
 
-        {
-        echo ".infected_*"
-        echo "log/access.log*"
-        echo "log/error.log*"
-        echo "*/core.[0-9]*"
-        echo "*/error_log"
-        echo "*/wp-content/updraft"
-        echo "*/wp-content/cache"
-        echo "*/wp-content/wpbackitup_backups"
-        echo "*/wp-content/uploads/ithemes-security"
-        echo "*/wp-content/uploads/wpallimport"
-        echo "*/wp-content/uploads/ShortpixelBackups"
+  {
+  echo ".infected_*"
+  echo "log/access.log*"
+  echo "log/error.log*"
+  echo "*/core.[0-9]*"
+  echo "*/error_log"
+  echo "*/wp-content/updraft"
+  echo "*/wp-content/cache"
+  echo "*/wp-content/wpbackitup_backups"
+  echo "*/wp-content/uploads/ithemes-security"
+  echo "*/wp-content/uploads/wpallimport"
+  echo "*/wp-content/uploads/ShortpixelBackups"
+  } > "$BSPATH"/rsync/exclude
 
-        } > "$BSPATH"/rsync/exclude
-else
-        :
 fi
 
+if [ -f /proc/vz/veinfo ]; then
+  remote=y
+  BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
+elif ! grep -qs '/backup ' /proc/mounts && ! grep destination=remote "$BSPATH"/backupinfo > /dev/null ; then
+  echo "Make sure to set destination=remote in "${BSPATH}"/backupinfo if supposed to be remote backups." | mail -s "Backup drive not mounted in $HOSTNAME" monitor@bigscoots.com
+  remote=y
+  BKUSER=wpo"${HOSTNAME//./}"
+elif ! grep -qs '/backup ' /proc/mounts && grep destination=remote "$BSPATH"/backupinfo > /dev/null ; then
+  remote=y
+  if [[ -n $(grep bkuser= "${BSPATH}"/backupinfo | sed 's/=/ /g' | awk '{print $2}') ]]; then
+    BKUSER=$(grep bkuser= "${BSPATH}"/backupinfo | sed 's/=/ /g' | awk '{print $2}')
+  else
+    BKUSER=wpo"${HOSTNAME//./}"
+  fi
+
+fi
+
+if  [[ $remote == y ]]; then
+  SSHOPTIONS="ssh -oStrictHostKeyChecking=no -i $HOME/.ssh/wpo_backups"
+  RSYNCLOCATION="$BKUSER@$BKSVR:"
+  if ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime' >/dev/null; [ $? -eq 255 ]
+  then
+    echo "Mark for Justin" | mail -s "$HOSTNAME- WPO failed to SSH to backup server." monitor@bigscoots.com
+    exit 1
+  fi
+else
+  RSYNCLOCATION=/backup/
+fi
 
 case $1 in
 manual)
 
   if [[ $2 == manual-* ]]; then
 
-  dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME)
+  dbname=$(wp $WPCLIFLAGS config get DB_NAME)
   /usr/bin/mysqldump "$dbname" | gzip > "$dbname".sql.gz
 
-  rsync -ah --stats \
-  -e "ssh -oStrictHostKeyChecking=no -i $HOME/.ssh/wpo_backups" \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$(dirname $PWD)" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
-  && ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
-  mv "incomplete_back-$date $2 \
-  && rm -f current \
-  && ln -s $2 current"
+    if  [[ $remote == y ]]; then
+      
+      rsync -ah \
+      -e "${SSHOPTIONS}" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="${BSPATH}"/rsync/exclude \
+      --link-dest=../current \
+      "$(dirname ${PWD})" "${RSYNCLOCATION}incomplete_back-${date}" 
 
+      ssh -oStrictHostKeyChecking=no -i "${HOME}"/.ssh/wpo_backups "${BKUSER}"@"${BKSVR}" "mv incomplete_back-$date ${2} && rm -f current && ln -s ${2} current"
+
+    else
+
+      rsync -ah \
+      -e "$SSHOPTIONS" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="$BSPATH"/rsync/exclude \
+      --link-dest=../current \
+      "$(dirname ${PWD})" "${RSYNCLOCATION}incomplete_back-${date}" 
+      
+      mv /backup/incomplete_back-"${date}" /backup/"${2}" && rm -f /backup/current && ln -s /backup/"${2}" /backup/current
+    fi
+    
   else
 
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
+  for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
+    dbname=$(wp $WPCLIFLAGS config get DB_NAME --path="$wpinstall")
     /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
-done
+  done
 
-  rsync -ah --stats \
-  -e "ssh -oStrictHostKeyChecking=no -i $HOME/.ssh/wpo_backups" \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$HOMEDIR" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
-  && ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
-  "mv incomplete_back-$date manual-$date \
-  && rm -f current \
-  && ln -s manual-$date current"
+    if  [[ $remote == y ]]; then
+      
+      rsync -ah \
+      -e "${SSHOPTIONS}" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="${BSPATH}"/rsync/exclude \
+      --link-dest=../current \
+      "${HOMEDIR}" "${RSYNCLOCATION}incomplete_back-${date}" 
 
+      ssh -oStrictHostKeyChecking=no -i "${HOME}"/.ssh/wpo_backups "${BKUSER}"@"${BKSVR}" "mv incomplete_back-$date manual-${date} && rm -f current && ln -s manual-${date} current"
+
+    else
+      
+      rsync -ah \
+      -e "$SSHOPTIONS" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="$BSPATH"/rsync/exclude \
+      --link-dest=../current \
+      "${HOMEDIR}" "${RSYNCLOCATION}incomplete_back-${date}" 
+      
+      mv /backup/incomplete_back-"${date}" /backup/back-"${date}" && rm -f /backup/current && ln -s /backup/"manual-${date}" /backup/current
+    fi
   fi
 
 ;;
@@ -89,13 +132,13 @@ delete)
   if [[ $2 == manual-* ]]; then
 
  mkdir -p "$HOMEDIR"/.empty
- rsync -a --stats \
- -e "ssh -oStrictHostKeyChecking=no -i $HOME/.ssh/wpo_backups" \
+ rsync -a \
+ -e "$SSHOPTIONS" \
  --ignore-errors \
  --delete \
- "$HOMEDIR"/.empty/ "$BKUSER"@"$BKSVR":$2/"$(dirname $PWD | sed 's/\// /g' | awk '{print $4}')"
+ "$HOMEDIR"/.empty/ "$BKUSER"@"$BKSVR":"$2"/"$(dirname "$PWD" | sed 's/\// /g' | awk '{print $4}')"
 
- ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "rmdir -p $2/$(dirname $PWD | sed 's/\// /g' | awk '{print $4}')"
+ ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "rmdir -p $2/$(dirname "$PWD" | sed 's/\// /g' | awk '{print $4}')"
 
 
 else
@@ -109,152 +152,42 @@ fi
 
 for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
+    dbname=$(wp $WPCLIFLAGS config get DB_NAME --path="$wpinstall")
     /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
 done
 
-rsync -ah --stats \
-  -e "ssh -oStrictHostKeyChecking=no -i $HOME/.ssh/wpo_backups" \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$HOMEDIR" "$BKUSER"@"$BKSVR":incomplete_back-"$date" \
-  && ssh -oStrictHostKeyChecking=no -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" \
-  "mv incomplete_back-$date back-$date \
-  && rm -f current \
-  && ln -s back-$date current"
+    if  [[ $remote == y ]]; then
+      
+      rsync -ah \
+      -e "${SSHOPTIONS}" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="${BSPATH}"/rsync/exclude \
+      --link-dest=../current \
+      "${HOMEDIR}" "${RSYNCLOCATION}incomplete_back-${date}" 
+
+      ssh -oStrictHostKeyChecking=no -i "${HOME}"/.ssh/wpo_backups "${BKUSER}"@"${BKSVR}" "mv incomplete_back-$date back-${date} && rm -f current && ln -s back-${date} current"
+
+    else
+      
+      rsync -ah \
+      -e "$SSHOPTIONS" \
+      --ignore-errors \
+      --delete \
+      --delete-excluded \
+      --exclude-from="$BSPATH"/rsync/exclude \
+      --link-dest=../current \
+      "${HOMEDIR}" "${RSYNCLOCATION}incomplete_back-${date}" 
+      
+      mv /backup/incomplete_back-"${date}" /backup/back-"${date}" && rm -f /backup/current && ln -s /backup/back-"${date}" /backup/current
+    fi
 
 ;;
 esac
 
-
 for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
    do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
+    dbname=$(wp $WPCLIFLAGS config get DB_NAME --path="$wpinstall")
     rm -f "$wpinstall$dbname".sql.gz
 done
-
-else
-
-  if ! grep -qs '/backup ' /proc/mounts ; then
-   echo "Backup drive not mounted in $HOSTNAME" | mail -s "Backup drive not mounted in $HOSTNAME" monitor@bigscoots.com
-   exit
-fi
-
-if [ ! -f "$BSPATH"/rsync/exclude ]; then
-        mkdir -p "$BSPATH"/rsync
-
-        {
-        echo "log/access.log*"
-        echo "log/error.log*"
-        echo "*/core.[0-9]*"
-        echo "*/error_log"
-        echo "*/wp-content/updraft"
-        echo "*/wp-content/cache"
-        echo "*/wp-content/wpbackitup_backups"
-        echo "*/wp-content/uploads/ithemes-security"
-        echo "*/wp-content/uploads/wpallimport"
-        echo "*/wp-content/uploads/ShortpixelBackups"
-
-        } > "$BSPATH"/rsync/exclude
-else
-        :
-fi
-
-
-case $1 in
-manual)
-
-  if [[ $2 == manual-* ]]; then
-
-  dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME)
-  /usr/bin/mysqldump "$dbname" | gzip > "$dbname".sql.gz
-
-  rsync -ah --stats \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$(dirname $PWD)" /backup/incomplete_back-"$date"
-
-  mv /backup/incomplete_back-$date /backup/$2 \
-  && rm -f /backup/current \
-  && ln -s /backup/$2 /backup/current
-
-  else
-
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
-   do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
-    /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
-done
-
-  rsync -ah --stats \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$HOMEDIR" /backup/incomplete_back-"$date"
-
-  mv /backup/incomplete_back-$date /backup/manual-$date \
-  && rm -f /backup/current \
-  && ln -s /backup/manual-$date /backup/current
-
-  fi
-
-;;
-delete)
-
-  if [[ $2 == manual-* ]]; then
-
- mkdir -p "$HOMEDIR"/.empty
- rsync -a --stats \
- --ignore-errors \
- --delete \
- "$HOMEDIR"/.empty/ /backup/$2/"$(dirname $PWD | sed 's/\// /g' | awk '{print $4}')"
-
- rm -rf "/backup/$2"
-
-
-else
-
-  echo "Make sure to specify a manual backup folder name."
-
-fi
-
-;;
-*)
-
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
-   do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
-    /usr/bin/mysqldump "$dbname" | gzip > "$wpinstall$dbname".sql.gz
-done
-
-rsync -ah --stats \
-  --ignore-errors \
-  --delete \
-  --delete-excluded \
-  --exclude-from="$BSPATH"/rsync/exclude \
-  --link-dest=../current \
-  "$HOMEDIR" /backup/incomplete_back-"$date"
-  
-  mv /backup/incomplete_back-$date /backup/back-$date \
-  && rm -f /backup/current \
-  && ln -s /backup/back-$date /backup/current
-
-;;
-esac
-
-
-for wpinstall in $(find /home/nginx/domains/*/public/ -type f -name wp-config.php | sed 's/wp-config.php//g')
-   do
-    dbname=$(wp --allow-root --skip-plugins --skip-themes config get DB_NAME --path="$wpinstall")
-    rm -f "$wpinstall$dbname".sql.gz
-done
-
-fi
