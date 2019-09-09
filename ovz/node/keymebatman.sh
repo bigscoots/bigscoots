ea9efa60879eaeb6031c9188d6847a3eb9b05946
Justin catello <justin@bigscoots.com>
2019-09-09 13:15:30 -0400

    Update keymebatman.sh

--- ovz/node/keymebatman.sh
+++ ovz/node/keymebatman.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# /bin/bash
 
 if [[ ! -f /vz/root/"$1"/root/.ssh/authorized_keys ]]; then
 
@@ -14,34 +14,49 @@ office01='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAn+cOO+QgKiuntfsPmJ8NtUsGNmOlT3LKjR
 office02='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAx/wguQjxQ/h3JD6Tit6hHvXZFszRnQQMMVOEIvn0WUxk1+O6VSKu1soc9E+gLg1PK8tUesgFyluRAjewyR8JTbBiP5ZCDddt2wyuBV8qEtJV7og96gzqABp28CY54f0ER8JBF7dtB1cjawliL0CquiJnBhWO4Q4VbBTr/EXZghPtWJKHGBwX7ziZKxcsSpJfrCbYbU1caUkOjkHSNdnKX6KhHK6pCaL8b27sDBwqZrv+YGfheQXnXjiEGW8/oJ8mSP6mawVlxFocGCZtfrjKsr5zDREalLmOAdXsFw/evID95tqyRZt4V5eirvtCA5P1N7+6oTDJ2XvCkjUtHSrzHXXZ5z6UkTJqCaqC3bRVbxVRkFWjxYqLBZe8YTBzwOoUXVhP2kYPxz97hKhblhHWpO5R1GtT0ragVdjeXxtLWgs2eTqmGQat4x4PxeEAUOjxeY48UBMRG50XCHkdVylZrgwaDBr1IV3lCZc8BtDJKT1QKygIZkHVQfaqfvtQ1oFB6Nx5SNzJ7mHvmIQvaj+tWSOTBdIny6DO2RhPCJBz3UNUDUuul9mw1j5Gsv0VvudFDz4DxOjGUlk519KoeNyRvBuDRwVb51HUGX/4lykHfaG7KMmy6V35JGEzna4Voy6VVQr2xp2eNYlbw15UcFfA1BXRWaPmYiNGeDXvWn8QZ/k= rsa-key-20170802'
 office03='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAjOtX/QuK+gv+yxQRvCRe3gFDvCR3qmjByRAbDq8I3FKBNLMUpDARMea8ISR/D/xgD1s30WWCXTtUQK9Qem0XSYSn9qdaKp30f7j5APfoL8bAect0i4XR/RpJBTbuH1Mt1WMaqKB5w8cuo7Rwo3dRE7iUZKlSjJFpofQ+hKAWFdnu82MgmetzbQtvR2Ta1ymLul2LK3bluy0tovyB4cWEFGFUwayK999tEvXJ3+T3PxEonVSUS2Ay3xfXJwK+yIigU/MQqf72bKlMRhGEuLnozlYwm5y97qJFKPIDSp4YN8ztmBeKLTBvQkSD32HctxKY4z2BzTev7Ip1Xhil6DDPY7Y/PoQwQ+xBP6jk6OpJud5P49lHIT16obkSW9L8fD5SHT+Ov7AJv0/cclY2VBbJBPKjCy5q+qeiVMSbGkAcRLp40UTTtWkFP6nmWjfPK1sytco5dy1GhoC6mwPrwLmq+mvMpa721NVpcw25/G7o7zXBXZ56i/7ImqlqwCa4/VNEioabhvM3zODLOfbqDXMeZVwIOAoshmAhGYLCm/+OdTi+J+D0+ub6k7EVze3h5/0c4rDYOib62Urp/G3ZUDSakLUj8KhyNLc5UaFbfQPD5ePiw2KQ9qO83Ikkt90oHjFQwW+vu8ribYgEsR/0qk8qTjFL8GYXRsRmqJaMyRFz18U= rsa-key-20170802'
 office04='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEA8Drkz4YtC2FOTYX3iBfkHcPTUoZm39ps3chvK/4jv5wdDtWPeVsPQ0/fmJDSCMqZ85ayuL0d6f7OhxJrhJvqE+AmVpx81O4BGSyjuWISTGg0AwMosNm4gEI8Tms4mP10iHA0nQL90OUYN3uFkp2a3/79bvYnJ2uHl2Sb50ygNOHnRGfds7FcembUCpGkitUqp+BRbRmGB1YFyT4NM+gvYhvdDyuZXgh37ReZpNKPLsqhBdpZOH57b708QUKltw0tLmbOD4e54i1mcyAmg60LZmVMJPfHUegUrb1SdCMCKuA+SKrzymdtCL38q4JmKbaNDc+F/evdfQhuh3fIrRcnMsY0PIf6CYFyVRidWafUCH3f0wTJYKwIOe8qIudEMULn8R1/6MmT4oQBy7NIe/UY7g+BzbXiuBXGgzEZIiyD8vCJym4XcCSL7heFR4Fe4mac1YjDQKoaV2reMbW6CcLGtIZyXnP2/xGWZxouQXr3OM/CAOX9fFxHIIj6OwabxugJu+uPmz+xty77U5YuRWiy73zWAftTZbMHfY187d6Q7KFVgxa3AWDZNYvKlv4vR3l2LOayNYG0Y6mxBkPGRfUnTXfxzYFSniieMPwCpxPyHRdAG68I1VLPd4dF7cZxu+UMJKSc1GfHew+ViResmExunCYCQGRVw3TO/Z+0a6MYWyE= dean'
-office05='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhD+XoZMHAXrpcR8MBtR7SGCe4Ii5G/PIKhKt/0RviOmF8yLJ3k8/ePmhA00QFM0DIRycgu0EatAYbzbrv9opJq8BMIaP2mpIbIrHZHwsIUKmoXCTcTu9ogOAp1Ke6pBIDYWA68dT62qRs05GJ9eRFRWBz6ymRKLlsEYFUS90nb3cH97cd+ILZv3qEGGC0nyPeoXVcX5MlmIU/S74ldNvvi0yo1UEH9/sKcc25wzKWQ0dzzGp5lpex4075NlrhlrMWysfe05XKKNYuDf3A7FSwMsgsFl5fVRbHCwqq3U1pb0RoNcWuzV5L33bLgpE57+RY403vVL3BMgbMNQfJ4io5 bassu@Thunderstorm'
+office05='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDGYqBDuXSS1yNjJWLilLFGTm/3+fxV7qz6JF1rSKEmNbW6wymmjIkBoVBzSxtQ5Dl3nZ7ELqAgHgAaFokrgJVootxYCY6dfH0JDc2fM5aCPo3fVzlrJqZA4TX7d0s6VcxQ1D1MDz75Rf8avLpvVkcrTWFO5gtNHbbhpG3LwU72EZdtZhevxT19XY4jotEc+iH6TSTlnGgoQlUzhbOkQ4o5SQZmdMCy4R0Zl3oyb0/VwOE89BLvHmnYOApRIsqS6lO/SI+Ifkez3tkzc8DBQcB0x8LGoKkbSJzx41fQAEPAP9WzNJ33hZSaxfKNTHY+D4qBfsxCkr0oNbESfoBENolKxwdG1c2TpJMS1Kx7sHHAAriS0HT72u4zz3cm5vyJm4Nh+KB+g2ECvE6qkGgHb6sZLUV0SH7JRUh6Zp8Wvy2nVM8GRtxSXdt5D/Vtp7cokd02g8l9bVBM3V3smn6mZyejR4wn6DKbQ2BTb4FgbA+CJoLIlZ+V0O+f1M9VnPrwTEX6AiW7own7Tp3YUQMJvSQcY+3lTgGOQY8J4yJTFokk8+H1UOzeFVtf6Wu/oSId39lqa2hvxdQsJ5NynIW1poMv8gX/R/UzPz5aWJq+XaRcSOANzjHsHoTcsMlS2cw0nivmDczkyV+mqYmQ664jBeBHVkEtmz80pC3iIvyWnMCO1Q== NathanLopez'
+  
+# remove
+
+if grep -q tim@localhost.localdomain /vz/root/"$1"/root/.ssh/authorized_keys; then
+
+	sed -i '/tim@localhost.localdomain/d ; /Office 4 - BigScoots.com/d' /vz/root/"$1"/root/.ssh/authorized_keys
+fi
+
+if grep -q bassu@Thunderstorm /vz/root/"$1"/root/.ssh/authorized_keys; then
+
+	sed -i '/bassu@Thunderstorm/d ; /Office 5 - BigScoots.com/d' /vz/root/"$1"/root/.ssh/authorized_keys
+fi
+
+# add
 
 if ! grep -q "${wponexus}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# WPO NEXUS - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo from=\"67.202.70.147\" "${wponexus}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+
+	echo "# WPO NEXUS - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo from=\"67.202.70.147\" "${wponexus}" >> /vz/root/"$1"/root/.ssh/authorized_keys
 fi
 
 if ! grep -q "${office01}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# Office 1 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo "${office01}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "# Office 1 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "${office01}" >> /vz/root/"$1"/root/.ssh/authorized_keys
 fi
 
 if ! grep -q "${office02}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# Office 2 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo "${office02}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "# Office 2 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "${office02}" >> /vz/root/"$1"/root/.ssh/authorized_keys
 fi
 
 if ! grep -q "${office03}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# Office 3 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo "${office03}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "# Office 3 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "${office03}" >> /vz/root/"$1"/root/.ssh/authorized_keys
 fi
 
 if ! grep -q "${office04}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# Office 4 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo "${office04}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "# Office 4 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "${office04}" >> /vz/root/"$1"/root/.ssh/authorized_keys
 fi
 
-if ! grep -q "${office05}" /vz/root/"$1"/root/.ssh/authorized_keys; then
-        echo "# Office 5 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
-        echo "${office05}" >> /vz/root/"$1"/root/.ssh/authorized_keys
-fi
+ if ! grep -q "${office05}" /vz/root/"$1"/root/.ssh/authorized_keys; then
+	echo "# Office 5 - BigScoots.com" >> /vz/root/"$1"/root/.ssh/authorized_keys
+	echo "${office05}" >> /vz/root/"$1"/root/.ssh/authorized_keys
+ fi
