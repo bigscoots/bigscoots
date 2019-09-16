f1f997c6f3782b925ef2f4dcc6e566f059b8a63a
Justin catello <justin@bigscoots.com>
2019-09-16 08:31:00 -0400

    Update clone.sh

--- wpo/manage/clone.sh
+++ wpo/manage/clone.sh
@@ -9,35 +9,34 @@ destinationsite="$2"
 
 
 WPCLIFLAGS="--allow-root --skip-plugins --skip-themes"
-NGINX=$(command -v nginx)
+NGINX=$(which nginx)
 
 if [ ! -d "/home/nginx/domains/$sourcesite" ] && [ ! -d "/home/nginx/domains/$destinationsite" ]; then
-    # EC-5 "Domain doesn't exist."
     exit 2
 fi
 
 sourcesitedocroot=/home/nginx/domains/"${sourcesite}"/public
 destinationsitedocroot=/home/nginx/domains/"${destinationsite}"/public
 
-if ! sourcesitedb=$(wp ${WPCLIFLAGS} config get DB_NAME --path="${destinationsitedocroot}" 2>&1); then 
+if ! sourcesitedb=$(wp ${WPCLIFLAGS} config get DB_NAME --path=${sourcesitedocroot} 2>&1); then 
     exit 3
 fi
 
-if ! destinationsitedb=$(wp ${WPCLIFLAGS} config get DB_NAME --path="${destinationsitedocroot}" 2>&1); then 
+if ! destinationsitedb=$(wp ${WPCLIFLAGS} config get DB_NAME --path=${destinationsitedocroot} 2>&1); then 
     exit 3
 fi
 
-if [[ $sourcesitedb == "$destinationsitedb" ]]; then
-    if ! wp ${WPCLIFLAGS} config set DB_NAME "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path="${destinationsitedocroot}" 2>&1; then
+if [[ $sourcesitedb == $destinationsitedb ]]; then
+    if ! wp ${WPCLIFLAGS} config set DB_NAME "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path=${destinationsitedocroot} 2>&1; then
         exit
     fi
-    if ! wp ${WPCLIFLAGS} config set DB_USER "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path="${destinationsitedocroot}" 2>&1; then
+    if ! wp ${WPCLIFLAGS} config set DB_USER "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path=${destinationsitedocroot} 2>&1; then
         exit
     fi
-    if ! wp ${WPCLIFLAGS} config set DB_PASSWORD "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path="${destinationsitedocroot}" 2>&1; then
+    if ! wp ${WPCLIFLAGS} config set DB_PASSWORD "$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)" --path=${destinationsitedocroot} 2>&1; then
         exit
     fi
-    if ! wp ${WPCLIFLAGS} db create --path="${destinationsitedocroot}" 2>&1; then
+    if ! wp ${WPCLIFLAGS} db create 2>&1; then
         exit
     fi
 fi
@@ -57,17 +56,15 @@ rsync -ahv --delete \
 --exclude wp-content/wpbackitup_backups \
 "$sourcesitedocroot/" "$destinationsitedocroot/"
 
-wp ${WPCLIFLAGS} --quiet db reset "${destinationsitedocroot}"
-wp ${WPCLIFLAGS} --quiet db export - --path="${destinationsitedocroot}" | wp --quiet db import - --path="${destinationsitedocroot}"
+wp ${WPCLIFLAGS} --quiet db reset "${destinationsitedocroot}" 2>&1 
+wp ${WPCLIFLAGS} --quiet db export - --path="${sourcesitedocroot}" | wp --quiet db import - --path="${destinationsitedocroot}" 2>&1
 
-# FIX BUG WITH PHP NOTICES BEING SHOWN
+wp ${WPCLIFLAGS} config set table_prefix $(wp ${WPCLIFLAGS} config get table_prefix --path="${sourcesitedocroot}") 2>&1
 
-wp ${WPCLIFLAGS} config set table_prefix $(wp ${WPCLIFLAGS} config get table_prefix --path="${destinationsitedocroot}") 2>&1
-
-# Ended Here
-
-siteurl=$(wp option get siteurl --path="${destinationsitedocroot}" ${WPCLIFLAGS} | sed -r 's/https?:\/\///g')
-wp search-replace "//$siteurl" "//$destinationsite" --recurse-objects --skip-columns=guid --skip-tables=wp_users --path="${destinationsitedocroot}" ${WPCLIFLAGS}
+cd "$sourcesitedocroot/" || exit
+siteurl=$(wp option get siteurl --allow-root | sed -r 's/https?:\/\///g')
+cd "$destinationsitedocroot/" || exit
+wp search-replace "//$siteurl" "//$destinationsite" --recurse-objects --skip-columns=guid --skip-tables=wp_users ${WPCLIFLAGS}
 
 
 if [ -n "$3" ] && [ -n "$4" ]; then
@@ -85,13 +82,22 @@ if [ -n "$3" ] && [ -n "$4" ]; then
     fi
 fi
 
-chown -R nginx: /home/nginx/domains/"${destinationsite}"
+if [ -d "wp-content/plugins/wp-rocket" ]; then
+
+    rm -f wp-content/advanced-cache.php
+    wp plugin ${WPCLIFLAGS} deactivate wp-rocket
+    wp plugin ${WPCLIFLAGS} activate wp-rocket
+
+fi
+
+chown -R nginx: /home/nginx/domains/$destinationsite
 
 redis-cli flushall
 
-if "$NGINX" -t > /dev/null 2>&1; then
-    npreload > /dev/null 2>&1
-else
-    "$NGINX" -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail during staging request -  $HOSTNAME" monitor@bigscoots.com
-    exit 1
-fi
+"$NGINX" -t > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+                npreload > /dev/null 2>&1
+        else
+                "$NGINX" -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail during staging request -  $HOSTNAME" monitor@bigscoots.com
+                exit 1
+    fi
