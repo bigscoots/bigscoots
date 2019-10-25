87b9d6f52b5a5cd266dda3f76107ff961a41bd83
Justin catello <justin@bigscoots.com>
2019-10-25 17:26:59 -0400

    Update sitelock.sh

--- wpo/manage/sitelock.sh
+++ wpo/manage/sitelock.sh
@@ -20,16 +20,15 @@ domain="$2"
 if [[ $1 == off ]]; then
 
     if  ! grep -q wpolocksite /usr/local/nginx/conf/conf.d/"$domain".ssl.conf; then
-        exit 3
+        exit 0
     fi
 
     if grep -q \#auth_basic_user_file /home/nginx/domains/"$domain"/wpolocksite /usr/local/nginx/conf/conf.d/"$domain".ssl.conf; then
-        exit 3
+        exit 0
     fi
 
     if ! grep -q \#auth_basic_user_file /home/nginx/domains/"$domain"/wpolocksite /usr/local/nginx/conf/conf.d/"$domain".ssl.conf; then
         sed -i "s=auth_basic_user_file /home/nginx/domains/$domain/wpolocksite=#auth_basic_user_file /home/nginx/domains/$domain/wpolocksite=g" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
-        exit 3
     fi
 fi
 
@@ -48,24 +47,25 @@ if [[ $1 == on ]]; then
 
 # /bigscoots/wpo/manage/sitelock.sh on domain.com user pass
 
-if  ! grep -q wpolocksite /usr/local/nginx/conf/conf.d/"$domain".ssl.conf ; then
+    if  ! grep -q wpolocksite /usr/local/nginx/conf/conf.d/"$domain".ssl.conf ; then
 
-/usr/local/nginx/conf/htpasswd.sh create /home/nginx/domains/"$domain"/wpolocksite "$3" "$4" > /dev/null 2>&1
-sed -i "/location \/ {/a \  auth_basic_user_file /home/nginx/domains/$domain/wpolocksite;" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
-sed -i "/location \/ {/a \  auth_basic \"Private\";" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
+    /usr/local/nginx/conf/htpasswd.sh create /home/nginx/domains/"$domain"/wpolocksite "$3" "$4" > /dev/null 2>&1
+    sed -i "/location \/ {/a \  auth_basic_user_file /home/nginx/domains/$domain/wpolocksite;" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
+    sed -i "/location \/ {/a \  auth_basic \"Private\";" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
 
-else
+    else
 
-/usr/local/nginx/conf/htpasswd.sh create /home/nginx/domains/"$domain"/wpolocksite "$3" "$4" > /dev/null 2>&1
+    /usr/local/nginx/conf/htpasswd.sh create /home/nginx/domains/"$domain"/wpolocksite "$3" "$4" > /dev/null 2>&1
 
-fi
-
-fi
-
-    nginx -t > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-                npreload > /dev/null 2>&1
-        else
-                nginx -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail while enabling/disabling htpasswd protection -  $HOSTNAME" monitor@bigscoots.com
-                exit 1
     fi
+
+fi
+
+
+nginx -t > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+npreload > /dev/null 2>&1
+else
+nginx -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail while enabling/disabling htpasswd protection -  $HOSTNAME" monitor@bigscoots.com
+exit 1
+fi
