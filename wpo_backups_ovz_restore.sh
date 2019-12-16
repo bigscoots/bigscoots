3fc6d7657da0b16e52b8061c744674061f6ecb4a
Justin catello <justin@bigscoots.com>
2019-12-16 18:51:26 -0500

    Update wpo_backups_ovz_restore.sh

--- wpo_backups_ovz_restore.sh
+++ wpo_backups_ovz_restore.sh
@@ -11,25 +11,12 @@ MYSQLDUMP=$(which mysqldump)
 RSYNC=$(which rsync)
 CHOWN=$(which chown)
 
+if [ -f /proc/vz/veinfo ];
+then
 
-if [ -f /proc/vz/veinfo ]; then
-  remote=y
-  BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
-elif ! grep -qs '/backup ' /proc/mounts && ! grep destination=remote "$BSPATH"/backupinfo >/dev/null 2>&1 ; then
-  echo "Make sure to set destination=remote in "${BSPATH}"/backupinfo if supposed to be remote backups." | mail -s "Backup drive not mounted in $HOSTNAME" monitor@bigscoots.com
-  remote=y
-  BKUSER=wpo"${HOSTNAME//./}"
-elif ! grep -qs '/backup ' /proc/mounts && grep destination=remote "$BSPATH"/backupinfo >/dev/null 2>&1 ; then
-  remote=y
-  if [[ -n $(grep bkuser= "${BSPATH}"/backupinfo | sed 's/=/ /g' | awk '{print $2}') ]]; then
-    BKUSER=$(grep bkuser= "${BSPATH}"/backupinfo | sed 's/=/ /g' | awk '{print $2}')
-  else
-    BKUSER=wpo"${HOSTNAME//./}"
-  fi
-fi
+BKUSER=wpo$(awk '{print $1}' /proc/vz/veinfo)
 
-
- if [[ ! -f wp-config.php ]] ; then
+if [[ ! -f wp-config.php ]] ; then
 
  echo "Run this script within the public directory of the WP install, quiting..."
  exit
@@ -40,7 +27,7 @@ DOMAIN=$(echo "$(pwd)"| sed "s=/home/nginx/domains/==g ; s=/public==g")
 
 fi
 
-if [[ $remote == y ]] && ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime' >/dev/null ; [ $? -eq 255 ]
+if ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" 'uptime' >/dev/null ; [ $? -eq 255 ]
 then
   echo "Connection to backup server has failed."
   exit 1
@@ -62,14 +49,14 @@ fi
 # /bigscoots/wpo_backups_ovz_restore.sh h daily
 # This will list out backups in WPO panel format for daily backups
 
-if [[ $remote == y ]] && [ $2 = "daily" ]; then
+if [ $2 = "daily" ]; then
 
 ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "ls -1d */$DOMAIN | sed 's/incomplete_back-//g ; s/back-//g ; s/T/ /g ; s/_/:/g ; s/\/$DOMAIN//g' | grep -v 'current\|manual' | sed 's/$/;/g'"
 
 # /bigscoots/wpo_backups_ovz_restore.sh h manual
 # This will list out backups in WPO panel format for manual backups
 
-elif [[ $remote == y ]] && [ $2 = "manual" ]; then
+elif [ $2 = "manual" ]; then
 
 ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "ls -1d */$DOMAIN | sed 's/incomplete_back-//g ; s/back-//g ; s/T/ /g ; s/_/:/g ; s/\/$DOMAIN//g' | grep 'manual' | sed 's/manual-//g' | sed 's/$/;/g'"
 
@@ -81,14 +68,12 @@ fi
 ;;
 hh)
 
-if [[ $remote == y ]]; then 
-	ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "ls -1d */$DOMAIN | sed 's/\/$DOMAIN//g' |grep -v current"
+ssh -i "$HOME"/.ssh/wpo_backups "$BKUSER"@"$BKSVR" "ls -1d */$DOMAIN | sed 's/\/$DOMAIN//g' |grep -v current"
 
-	echo
-	echo "Example rsync command - You should run a backup before proceeding"
-	echo "# rsync -ahv -e \"ssh -i $HOME/.ssh/wpo_backups\" --delete $BKUSER@$BKSVR:$(ssh -i $HOME/.ssh/wpo_backups $BKUSER@$BKSVR 'echo $HOME')/$(ssh -i $HOME/.ssh/wpo_backups $BKUSER@$BKSVR "ls -1d */$DOMAIN | sed 's/\/$DOMAIN//g' |grep -v current" | tail -1)/$DOMAIN/public/ $(pwd)/"
-	echo
-fi
+echo
+echo "Example rsync command - You should run a backup before proceeding"
+echo "# rsync -ahv -e \"ssh -i $HOME/.ssh/wpo_backups\" --delete $BKUSER@$BKSVR:$(ssh -i $HOME/.ssh/wpo_backups $BKUSER@$BKSVR 'echo $HOME')/$(ssh -i $HOME/.ssh/wpo_backups $BKUSER@$BKSVR "ls -1d */$DOMAIN | sed 's/\/$DOMAIN//g' |grep -v current" | tail -1)/$DOMAIN/public/ $(pwd)/"
+echo
 
 # /bigscoots/wpo_backups_ovz_restore.sh restore ${BACKUP}
 # This will restore the current site from the date specified, must be in the public directory.
