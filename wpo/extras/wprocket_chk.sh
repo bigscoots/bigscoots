9b8af1234e43f6a096e59ee7f5e9f867095f79a1
Justin catello <justin@bigscoots.com>
2019-12-20 17:55:53 -0500

    Update wprocket_chk.sh

--- wpo/extras/wprocket_chk.sh
+++ wpo/extras/wprocket_chk.sh
@@ -2,6 +2,7 @@
 
 domain="${1}"
 
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
 sslconf=$(echo /usr/local/nginx/conf/conf.d/"${domain}".ssl.conf)
 wpinstall=/home/nginx/domains/"${domain}"/public
 
@@ -10,10 +11,16 @@ if [ ! -d "${wpinstall}" ]; then
   exit 1
 fi
 
-if [ -d "${wpinstall}/wp-content/plugins/wp-rocket" ]; then
+if [ $2 = -c ]; then
+  if ! wp ${WPCLIFLAGS} plugin is-active wp-rocket --path="${wpinstall}"; then
+    exit 5 # installed but not active
+  fi
+fi
+
+
+if wp ${WPCLIFLAGS} plugin is-installed wp-rocket --path="${wpinstall}"; then
 
   if [ ! -d "/usr/local/nginx/conf/rocket-nginx" ]; then
-
     bringmeback=$(pwd)
     cd /usr/local/nginx/conf/ || exit
     git clone https://github.com/maximejobin/rocket-nginx.git
@@ -21,34 +28,34 @@ if [ -d "${wpinstall}/wp-content/plugins/wp-rocket" ]; then
     cp rocket-nginx.ini.disabled rocket-nginx.ini
     php rocket-parser.php
     cd "$bringmeback" || exit
-
+    reloadconfig=1
   fi
 
   if ! grep -q "rocket-nginx/default.conf" "${sslconf}" ; then
-
     sed -i '/rediscache_/a\ \ include /usr/local/nginx/conf/rocket-nginx/default.conf\;' "${sslconf}"
-
+    reloadconfig=1
   fi
 
-  sed -i 's/#include \/usr\/local\/nginx\/conf\/rocket-nginx\/default.conf;/include \/usr\/local\/nginx\/conf\/rocket-nginx\/default.conf;/g' "${sslconf}"
+  if grep -q "#include /usr/local/nginx/conf/rocket-nginx/default.conf;" "${sslconf}" ; then
+    sed -i 's/#include \/usr\/local\/nginx\/conf\/rocket-nginx\/default.conf;/include \/usr\/local\/nginx\/conf\/rocket-nginx\/default.conf;/g' "${sslconf}"
+    reloadconfig=1
+  fi
 
   for i in wpsupercache_ wpcacheenabler_ rediscache_ ; do
-
     if [[ ! $(grep $i "${sslconf}") =~ ^# ]]; then
-
       sed -i "/$i/s/#\?include /#include /g" "${sslconf}"
-
+      reloadconfig=1
     fi
-
   done
 
   sed -i 's/#\?try_files /#try_files /g ; s/#try_files \$uri \$uri\/ \/index.php?\$/try_files \$uri \$uri\/ \/index.php?\$/g' "${sslconf}"
 
+  if [ $reloadconfig == 1 ]; then
   "$NGINX" -t > /dev/null 2>&1
     if [ $? -eq 0 ]; then
                 npreload > /dev/null 2>&1
     else
                 "$NGINX" -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail when running wprocket_chk.sh on $domain -  $HOSTNAME" monitor@bigscoots.com
     fi
-
+  fi
 fi
