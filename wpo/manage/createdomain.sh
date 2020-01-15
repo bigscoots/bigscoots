0b43f987313c2f3bd8af18199bc47c092028d856
Justin catello <justin@bigscoots.com>
2020-01-14 21:35:43 -0500

    Update createdomain.sh

--- wpo/manage/createdomain.sh
+++ wpo/manage/createdomain.sh
@@ -22,7 +22,7 @@ domainip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([
 
 if [ -d /home/nginx/domains/"$domain" ]; then
   echo "$domain already exists on the server."
-  exit 1
+  exit
 fi
 
 if [ "$2" == fresh ]; then
@@ -36,7 +36,13 @@ else
 
 fi
 
+sed -i "/\/usr\/local\/nginx\/conf\/503include-only.conf/a \  include \/usr\/local\/nginx\/conf\/wpincludes\/$domain\/redirects.conf;" /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
+sed -i 's/include \/usr\/local\/nginx\/conf\/autoprotect/#include \/usr\/local\/nginx\/conf\/autoprotect/g' /usr/local/nginx/conf/conf.d/"$domain".ssl.conf
+crontab -l | grep -v '/root/tools/wp_updater'  | crontab -
+
 sed "s/REPLACEDOMAIN/$domain/g ; s/REPLACEIP/$domainip/g" /bigscoots/wpo/extras/dnszone.txt > /home/nginx/domains/"$domain"/"$domain"-dnszone.txt
+
+touch /usr/local/nginx/conf/wpincludes/"$domain"/redirects.conf
 
 if [ -f /home/nginx/domains/"$domain"/.fresh ]; then
   cat /home/nginx/domains/"$domain"/.fresh | mail -s "$domain has been successfully created on  $HOSTNAME - DNS attached" -a /home/nginx/domains/"$domain"/"$domain"-dnszone.txt monitor@bigscoots.com
