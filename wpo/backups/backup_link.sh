2ab48d22878c44022a9c8d42404cdae531b7f7f3
Justin catello <justin@bigscoots.com>
2020-01-17 14:28:23 -0500

    Create backup_link.sh

---
+++ wpo/backups/backup_link.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Generate backup and provide a link that expires after 48 hours.
+
+rando1=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+rando2=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+backup=$1
+
+if [ -z "$backup" ]; then 
+	echo "need filename"
+	exit
+fi
+
+mkdir -p /var/www/html/"$rando1"/"$rando2"/
+mv "$backup" /var/www/html/"$rando1"/"$rando2"/
+
+screen -dmS "$backup" bash -c "sleep 5 ; rm -rf /var/www/html/$rando1"
+
+echo http://"$HOSTNAME"/"$rando1"/"$rando2"/"$backup"
