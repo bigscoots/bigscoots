ecca3e1592954f678c8fbeb97d202c3da84c2f6c
Justin catello <justin@bigscoots.com>
2020-01-17 14:58:06 -0500

    Update staging.sh

--- wpo/manage/staging.sh
+++ wpo/manage/staging.sh
@@ -14,14 +14,14 @@ sourcedomainsize="$(du --exclude-from="${BSPATH}"/rsync/"${destinationdomain}"/e
 freespace="$(df -k / | tr -s ' ' | cut -d" " -f 4 | grep -v Available)"
 percentofreespace=$((sourcedomainsize*100/freespace))
 
+if [ ! -d /home/nginx/domains/"$destinationdomain" ] && [ "$percentofreespace" -ge 45 ] ; then
+        echo "$sourcedomain is using  $percentofreespace% of the available free space, we require less than 40% to prevent server from running out of space." | mail -s "WPO Staging attemped but not enough disk space on  $HOSTNAME" monitor@bigscoots.com
+        exit 1
+fi
+
 if [ ! -d /home/nginx/domains/"$destinationdomain" ] ; then
         echo "$destinationdomain doesn't exist, creating it..."
         /bigscoots/wpo/manage/createdomain.sh "$destinationdomain"
-fi
-
-if [ "$percentofreespace" -ge 45 ] ; then
-        echo "$sourcedomain is using  $percentofreespace% of the available free space, we require less than 40% to prevent server from running out of space." | mail -s "WPO Staging attemped but not enough disk space on  $HOSTNAME" monitor@bigscoots.com
-        exit 1
 fi
 
 /bigscoots/wpo/manage/clone.sh "$sourcedomain" "$destinationdomain" dev dev
