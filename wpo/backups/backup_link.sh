14b6f5fee874628addc695ad98b097696ad714df
Justin catello <justin@bigscoots.com>
2020-02-04 15:36:43 -0500

    Update backup_link.sh

--- wpo/backups/backup_link.sh
+++ wpo/backups/backup_link.sh
@@ -26,4 +26,4 @@ $link"
 ( input  | split("|") ) as $keys |
 ( inputs | split("|") ) as $vals |
 [[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
-' <<<"$backupinfo"
+' <<<"$downloadinfo"
