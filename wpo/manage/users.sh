cc3cd75acbb0d1f56fde3f13a2729143f65f3927
Justin catello <justin@bigscoots.com>
2020-02-05 15:28:40 -0500

    Create users.sh

---
+++ wpo/manage/users.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+domain="$2"
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
+
+
+case $1 in
+list_admins)
+
+# /bigscoots/wpo/manage/users.sh list_admins ${DOMAIN} ${EMAIL}
+
+wp ${WPCLIFLAGS} user list --role=administrator --fields=ID,user_login,user_email,user_registered --format=json --path=/home/nginx/domains/"$domain"/public
+
+;;
+change_email)
+
+# /bigscoots/wpo/manage/users.sh change_email ${DOMAIN} ${ID} ${EMAIL}
+
+id="$3"
+email="$4"
+
+wp ${WPCLIFLAGS} user update "$id" --user_email="$email" --path=/home/nginx/domains/"$domain"/public
+
+;;
+change_password)
+
+# /bigscoots/wpo/manage/users.sh change_password ${DOMAIN} ${ID} ${PASSWORD}
+
+id="$3"
+password="$4"
+
+wp ${WPCLIFLAGS} user update "$id" --user_pass="$password" --path=/home/nginx/domains/"$domain"/public
+
+;;
+esac
