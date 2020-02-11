d0799df443d9727296e3132ff9755f7fc6573beb
Justin catello <justin@bigscoots.com>
2020-02-11 14:13:40 -0500

    Create plugins.sh

---
+++ wpo/manage/plugins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+domain="$2"
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
+
+
+case $1 in
+list_plugins)
+
+# /bigscoots/wpo/manage/plugins.sh list_plugins ${DOMAIN}
+
+wp ${WPCLIFLAGS} plugin list --fields=name,title,status,update,version,update_version --format=json --path=/home/nginx/domains/"$domain"/public
+
+;;
+update_plugin)
+
+# /bigscoots/wpo/manage/plugins.sh update_plugin ${DOMAIN} ${PLUGIN}
+
+plugin="$3"
+
+wp ${WPCLIFLAGS} plugin update "$plugin" --path=/home/nginx/domains/"$domain"/public
+
+;;
+toggle_plugin)
+
+# /bigscoots/wpo/manage/plugins.sh toggle_plugin ${DOMAIN} ${PLUGIN}
+
+plugin="$3"
+
+wp ${WPCLIFLAGS} plugin toggle "$plugin" --path=/home/nginx/domains/"$domain"/public
+
+;;
+uninstall_plugin)
+
+# /bigscoots/wpo/manage/plugins.sh uninstall_plugin ${DOMAIN} ${PLUGIN}
+
+plugin="$3"
+
+wp ${WPCLIFLAGS} plugin uninstall "$plugin" --deactivate --path=/home/nginx/domains/"$domain"/public
+
+;;
+esac
