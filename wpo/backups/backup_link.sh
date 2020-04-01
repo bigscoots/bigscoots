cb023a18854a7676da29782f5c546acdab406b29
Justin catello <justin@bigscoots.com>
2020-03-31 23:17:43 -0400

    Update backup_link.sh

--- wpo/backups/backup_link.sh
+++ wpo/backups/backup_link.sh
@@ -2,28 +2,39 @@
 
 # Generate backup and provide a link that expires after 48 hours.
 
-rando1=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
-rando2=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
-backup=$1
+RANDO1=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+RANDO2=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+BACKUP=$1
 
-if [ -z "$backup" ]; then 
+if [ -z "${BACKUP}" ]; then 
 	echo "need filename"
 	exit
 fi
 
-mkdir -p /var/www/html/"$rando1"/"$rando2"/
-mv "$backup" /var/www/html/"$rando1"/"$rando2"/
+if [[ $2 == local ]]; then
+	mkdir -p /usr/local/nginx/html/"${RANDO1}"/"${RANDO2}"/
+	ln -s "${PWD}"/"${BACKUP}" /usr/local/nginx/html/"${RANDO1}"/"${RANDO2}"/"${BACKUP}"
+	screen -dmS "${BACKUP}" bash -c "sleep 172800 ; [ -d /usr/local/nginx/html/${RANDO1} ] && rm -rf /usr/local/nginx/html/${RANDO1} ; [ -f ${PWD}/${BACKUP} ] && rm -f ${PWD}/${BACKUP}"
 
-screen -dmS "$backup" bash -c "sleep 172800 ; rm -rf /var/www/html/$rando1"
+	echo "Path: ${PWD}/${BACKUP}"
+	echo "Sympath: /usr/local/nginx/html/${RANDO1}/${RANDO2}/${BACKUP}"
+	link="https://$HOSTNAME/${RANDO1}/${RANDO2}/${BACKUP}"
+else
 
-# echo "Path: /var/www/html/$rando1/$rando2/$backup"
-link="http://$HOSTNAME/$rando1/$rando2/$backup"
+	mkdir -p /var/www/html/"${RANDO1}"/"${RANDO2}"/
+	mv "${BACKUP}" /var/www/html/"${RANDO1}"/"${RANDO2}"/
 
-downloadinfo="DownloadLink
-$link"
+	screen -dmS "${BACKUP}" bash -c "sleep 172800 ; rm -rf /var/www/html/${RANDO1}"
 
-  jq -Rn '
-( input  | split("|") ) as $keys |
-( inputs | split("|") ) as $vals |
-[[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
-' <<<"$downloadinfo"
+	# echo "Path: /var/www/html/${RANDO1}/${RANDO2}/${BACKUP}"
+	link="http://$HOSTNAME/${RANDO1}/${RANDO2}/${BACKUP}"
+
+	downloadinfo="DownloadLink
+	$link"
+	
+	  jq -Rn '
+	( input  | split("|") ) as $keys |
+	( inputs | split("|") ) as $vals |
+	[[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
+	' <<<"$downloadinfo"
+fi
