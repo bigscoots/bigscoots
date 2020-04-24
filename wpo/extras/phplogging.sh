fc80808f2542a170e6474c12dd89e895d0a022d3
Justin catello <justin@bigscoots.com>
2020-04-24 14:17:14 -0400

    Create phplogging.sh
    per domain php logging

---
+++ wpo/extras/phplogging.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Gives us PHP logging per vhost even though one pool
+
+# disable global error log which prevents .user.ini from working
+
+if grep -o '^php_admin_value\[error_log\]' /usr/local/etc/php-fpm.conf; then
+ sed -i 's/php_admin_value\[error_log\]/;php_admin_value\[error_log\]/g' /usr/local/etc/php-fpm.conf
+fi
+
+for WPATH in $(find /home/nginx/domains/*/public -maxdepth 1 -type d -name public); do
+
+	PHPLOGFILE=$(echo "$WPATH" | sed 's/\/public/\/log\/php_error.log/g')
+
+	if [ ! -f "${WPATH}"/.user.ini ]; then
+		touch "${WPATH}"/.user.ini
+	fi
+
+	if grep -q "^error_log" "${WPATH}"/.user.ini; then
+	 	sed -i "/^error_log/c\error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini
+ 	else
+ 		echo "error_log = ${PHPLOGFILE}" >> "${WPATH}"/.user.ini
+ 	fi
+
+done
