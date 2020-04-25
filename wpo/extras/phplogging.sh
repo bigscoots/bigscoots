268f9dd4e2d069ab3f4c296188d6ed5e35a082ec
Justin catello <justin@bigscoots.com>
2020-04-25 13:28:40 -0400

    Update phplogging.sh

--- wpo/extras/phplogging.sh
+++ wpo/extras/phplogging.sh
@@ -4,39 +4,45 @@
 
 # disable global error log which prevents .user.ini from working
 
+RELOADFPM=0
+
 if grep -o '^php_admin_value\[error_log\]' /usr/local/etc/php-fpm.conf >/dev/null 2>&1; then
  sed -i 's/php_admin_value\[error_log\]/;php_admin_value\[error_log\]/g' /usr/local/etc/php-fpm.conf
+ RELOADFPM=1
 fi
 
 # find all available paths to place the .user.ini
 
 for WPATH in $(find /home/nginx/domains/*/public -maxdepth 1 -type d -name public); do
 
-	# Set the full path including filename of the PHP error_log.
+        # Set the full path including filename of the PHP error_log.
 
-	PHPLOGFILE=$(echo "$WPATH" | sed 's/\/public/\/log\/php_error.log/g')
+        PHPLOGFILE=$(echo "$WPATH" | sed 's/\/public/\/log\/php_error.log/g')
 
-	# if the .user.ini doesn't exist it will create it, if it fails to create, it will send you an email.
+        # if the .user.ini doesn't exist it will create it, if it fails to create, it will send you an email.
 
-	if [ ! -f "${WPATH}"/.user.ini ]; then
-		if ! touch "${WPATH}"/.user.ini; then
-			echo "" | mail -s "Failed to create ${WPATH}/.user.ini on  $HOSTNAME - leave for justin" monitor@bigscoots.com
-		fi
-	fi
+        if [ ! -f "${WPATH}"/.user.ini ]; then
+                if ! touch "${WPATH}"/.user.ini; then
+                        echo "" | mail -s "Failed to create ${WPATH}/.user.ini on  $HOSTNAME - leave for justin" monitor@bigscoots.com
+                fi
+        fi
 
-	# will replace error_log option that specifies the location of the log file if it already exists but does not match the correct path and then reloads php-fpm
+        # will replace error_log option that specifies the location of the log file if it already exists but does not match the correct path and then reloads php-fpm
 
-	if grep -q "^error_log" "${WPATH}"/.user.ini && ! grep -q "error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini >/dev/null 2>&1; then
-	 	sed -i "/^error_log/c\error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini
-	 	fpmreload
- 	else
+        if grep -q "^error_log" "${WPATH}"/.user.ini && ! grep -q "error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini >/dev/null 2>&1; then
+                sed -i "/^error_log/c\error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini
+                RELOADFPM=1
 
- 	# adds the error_log option if it doesn't exist and reloads php-fpm
- 		echo "error_log = ${PHPLOGFILE}" >> "${WPATH}"/.user.ini
- 		fpmreload
- 	fi
+        # adds the error_log option if it doesn't exist and reloads php-fpm
 
- 	# fix ownership of the .user.ini
- 	chown nginx: "${WPATH}"/.user.ini
+        elif ! grep -q "error_log = ${PHPLOGFILE}" "${WPATH}"/.user.ini; then
+                echo "error_log = ${PHPLOGFILE}" >> "${WPATH}"/.user.ini
+                RELOADFPM=1
+        fi
+
+        # fix ownership of the .user.ini
+        chown nginx: "${WPATH}"/.user.ini
 
 done
+
+[[ "${RELOADFPM}" = 1  ]] && fpmreload
