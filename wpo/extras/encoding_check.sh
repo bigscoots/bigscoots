63ab9c144f4ec714324765736c98e0f76d142d71
Justin catello <justin@bigscoots.com>
2020-05-11 12:53:00 -0400

    Update encoding_check.sh

--- wpo/extras/encoding_check.sh
+++ wpo/extras/encoding_check.sh
@@ -4,9 +4,12 @@
 
 WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
 
-echo 'This list can take a while so if you see enough just ctrl + c to stop the command it wont break anything'
-
 if [ "$(wp ${WPCLIFLAGS} db search '????' wp_posts post_content | grep -A1 _posts:post_content | grep [0-9]: | sed 's/:/  /g' | awk '{print $1}' | sort | uniq  | wc -l)" -gt 0 ]; then
+	echo
+	echo '--------------------------------------------------------------------------------------------------------'
+	echo 'This list can take a while so if you see enough just ctrl + c to stop the command it wont break anything'
+	echo '--------------------------------------------------------------------------------------------------------'
+	echo
 	echo "Potential broken encoding issues, please check the following posts:"
 	wp ${WPCLIFLAGS} db search '????' wp_posts post_content --stats | grep -A1 _posts:post_content | grep "[0-9]:" | sed 's/:/  /g' | awk '{print $1}' | sort | uniq \
 	| while read -r postid; do
