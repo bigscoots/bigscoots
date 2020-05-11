8fa53ef84de9f2c99ac2a25524259be147ffaad5
Justin catello <justin@bigscoots.com>
2020-05-11 12:51:07 -0400

    Update encoding_check.sh

--- wpo/extras/encoding_check.sh
+++ wpo/extras/encoding_check.sh
@@ -2,14 +2,16 @@
 
 # Check posts for possible encoding issues.
 
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
+
 echo 'This list can take a while so if you see enough just ctrl + c to stop the command it wont break anything'
 
-if [ "$(wp db search '????' wp_posts post_content | grep -A1 _posts:post_content | grep [0-9]: | sed 's/:/  /g' | awk '{print $1}' | sort | uniq  | wc -l)" -gt 0 ]; then
+if [ "$(wp ${WPCLIFLAGS} db search '????' wp_posts post_content | grep -A1 _posts:post_content | grep [0-9]: | sed 's/:/  /g' | awk '{print $1}' | sort | uniq  | wc -l)" -gt 0 ]; then
 	echo "Potential broken encoding issues, please check the following posts:"
-	wp db search '????' wp_posts post_content --stats | grep -A1 _posts:post_content | grep "[0-9]:" | sed 's/:/  /g' | awk '{print $1}' | sort | uniq \
+	wp ${WPCLIFLAGS} db search '????' wp_posts post_content --stats | grep -A1 _posts:post_content | grep "[0-9]:" | sed 's/:/  /g' | awk '{print $1}' | sort | uniq \
 	| while read -r postid; do
-		if wp post list --post_status=publish --post__in="${postid}" --quiet |grep -q "${postid}"; then
-			echo "$(wp option get siteurl)/$(wp post list --post__in="${postid}" --field=post_name)"
+		if wp ${WPCLIFLAGS} post list --post_status=publish --post__in="${postid}" --quiet |grep -q "${postid}"; then
+			echo "$(wp ${WPCLIFLAGS} option get siteurl)/$(wp ${WPCLIFLAGS} post list --post__in="${postid}" --field=post_name)"
 		fi
 	done
 fi
