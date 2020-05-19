0872a6c5d2d92cfc4de8af61b786d77da4491d5a
Justin catello <justin@bigscoots.com>
2020-05-19 07:45:09 -0400

    Update fixpermissions.sh

--- wpo/extras/fixpermissions.sh
+++ wpo/extras/fixpermissions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-chown -R nginx: /home/nginx/domains/ > /dev/null 2>&1
-find /home/nginx/domains/ -type f -exec chmod 644 {} \; > /dev/null 2>&1
-find /home/nginx/domains/ -type d -exec chmod 755 {} \; > /dev/null 2>&1
+DOMAIN=$1
+
+if [ -d /home/nginx/domains/"${DOMAIN}" ]; then
+  chown -R nginx: /home/nginx/domains/"${DOMAIN}" > /dev/null 2>&1
+  find /home/nginx/domains/"${DOMAIN}" -type f -exec chmod 644 {} \; > /dev/null 2>&1
+  find /home/nginx/domains/"${DOMAIN}" -type d -exec chmod 755 {} \; > /dev/null 2>&1
+fi
