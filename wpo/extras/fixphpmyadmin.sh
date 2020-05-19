45b2492e0a9b9a1734d3d8ff4e1708d43f118540
Justin catello <justin@bigscoots.com>
2020-05-19 07:28:08 -0400

    Update fixphpmyadmin.sh

--- wpo/extras/fixphpmyadmin.sh
+++ wpo/extras/fixphpmyadmin.sh
@@ -8,10 +8,10 @@ if grep "^cd /usr/local/nginx/html/" /root/tools/phpmyadmin_update.sh >/dev/null
 	$(grep "^cd /usr/local/nginx/html/" /root/tools/phpmyadmin_update.sh)
 	if echo $(pwd) |grep -q /usr/local/nginx/html/*_mysqladmin*; then
 		rm -rf !("config.inc.php")
-		wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.zip
-		unzip phpMyAdmin-5.0.1-all-languages.zip
-		mv phpMyAdmin-5.0.1-all-languages/* .
-		rm -rf phpMyAdmin-5.0.1-all-languages*
+		wget https://github.com/phpmyadmin/phpmyadmin/archive/STABLE.zip
+		unzip STABLE.zip
+		mv phpmyadmin-STABLE/* .
+		rm -rf phpmyadmin-STABLE*
 		chown -R nginx: .
 		npreload
 	fi
