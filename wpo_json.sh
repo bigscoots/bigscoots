0f8c5687ee8b975ad8e67d0230cec8602d694088
Justin catello <justin@bigscoots.com>
2020-05-26 11:30:28 -0400

    Update wpo_json.sh

--- wpo_json.sh
+++ wpo_json.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BSPATH=/root/.bigscoots
+WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
 
 {
 
@@ -65,8 +66,8 @@ for domain in $(\ls /home/nginx/domains/) ; do
 pmaurl=$(grep "https://$HOSTNAME/[0-9]" $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" | sed 's/$/\//g' | tr -d " \t\n\r")
 pmauser=$(grep Username: $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | awk '{print $2}')
 pmapass=$(grep Password: $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | awk '{print $2}')
-pmadbuser=$(wp --allow-root config get DB_USER --path=/home/nginx/domains/"$domain"/public/)
-pmadbpass=$(wp --allow-root config get DB_PASSWORD --path=/home/nginx/domains/"$domain"/public/)
+pmadbuser=$(wp ${WPCLIFLAGS} config get DB_USER --path=/home/nginx/domains/"$domain"/public/)
+pmadbpass=$(wp ${WPCLIFLAGS} config get DB_PASSWORD --path=/home/nginx/domains/"$domain"/public/)
 
 vhostlog=$(grep -l "FTP username created for $domain" /root/centminlogs/centminmod*addvhost.log | xargs ls -rt | tail -1)
 grep -v "FTP Passive" "$vhostlog" | grep -C2 "FTP mode" > /tmp/tmpftp.txt
@@ -117,8 +118,8 @@ fi
 pmaurl=$(grep "https://$HOSTNAME/[0-9]" $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" | sed 's/$/\//g' | tr -d " \t\n\r")
 pmauser=$(grep Username: $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | awk '{print $2}')
 pmapass=$(grep Password: $(grep -rl $(echo /usr/local/nginx/html/*mysqladmin* | sed 's/\/usr\/local\/nginx\/html\///g' | head -1) /root/centminlogs/centminmod_phpmyadmin_install_*.log) | awk '{print $2}')
-pmadbuser=$(wp --allow-root config get DB_USER --path=/home/nginx/domains/"$domain"/public/)
-pmadbpass=$(wp --allow-root config get DB_PASSWORD --path=/home/nginx/domains/"$domain"/public/)
+pmadbuser=$(wp ${WPCLIFLAGS} config get DB_USER --path=/home/nginx/domains/"$domain"/public/)
+pmadbpass=$(wp ${WPCLIFLAGS} config get DB_PASSWORD --path=/home/nginx/domains/"$domain"/public/)
 
 vhostlog=$(grep -l "FTP username created for $domain" /root/centminlogs/centminmod*addvhost.log | xargs ls -rt | tail -1)
 grep -v "FTP Passive" "$vhostlog" | grep -C2 "FTP mode" > /tmp/tmpftp.txt
