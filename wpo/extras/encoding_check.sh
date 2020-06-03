d817614514e7bfad9be7a3dd74d2a081c0be1e73
Justin catello <justin@bigscoots.com>
2020-06-02 21:39:20 -0400

    Update encoding_check.sh

--- wpo/extras/encoding_check.sh
+++ wpo/extras/encoding_check.sh
@@ -3,15 +3,16 @@
 # Check posts for possible encoding issues.
 
 WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
+wdpprefix=$(wp ${WPCLIFLAGS} config get table_prefix)
 
-if [ "$(wp ${WPCLIFLAGS} db search '????' wp_posts post_content | grep -A1 _posts:post_content | grep [0-9]: | sed 's/:/  /g' | awk '{print $1}' | sort | uniq  | wc -l)" -gt 0 ]; then
+if [ "$(wp ${WPCLIFLAGS} db search '????' "${wdpprefix}posts" post_content | grep -A1 _posts:post_content | grep [0-9]: | sed 's/:/  /g' | awk '{print $1}' | sort | uniq  | wc -l)" -gt 0 ]; then
 	echo
 	echo '--------------------------------------------------------------------------------------------------------'
 	echo 'This list can take a while so if you see enough just ctrl + c to stop the command it wont break anything'
 	echo '--------------------------------------------------------------------------------------------------------'
 	echo
 	echo "Potential broken encoding issues, please check the following posts:"
-	wp ${WPCLIFLAGS} db search '????' wp_posts post_content --stats | grep -A1 _posts:post_content | grep "[0-9]:" | sed 's/:/  /g' | awk '{print $1}' | sort | uniq \
+	wp ${WPCLIFLAGS} db search '????' "${wdpprefix}posts" post_content --stats | grep -A1 _posts:post_content | grep "[0-9]:" | sed 's/:/  /g' | awk '{print $1}' | sort | uniq \
 	| while read -r postid; do
 		if wp ${WPCLIFLAGS} post list --post_status=publish --post__in="${postid}" --quiet |grep -q "${postid}"; then
 			echo "$(wp ${WPCLIFLAGS} option get siteurl)/$(wp ${WPCLIFLAGS} post list --post__in="${postid}" --field=post_name)"
