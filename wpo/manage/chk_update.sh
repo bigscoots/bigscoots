85635070d7ce305a64fe64c5b2bd84bbae5995e4
Justin catello <justin@bigscoots.com>
2020-07-11 15:51:32 -0400

    Create chk_update.sh

---
+++ wpo/manage/chk_update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -f /etc/centminmod-release ]; then 
+	
+	if ! crontab -l | grep -q wpo_update.sh; then 
+		crontab -l | { cat; echo "0 */6 * * * /usr/bin/cmupdate 2>/dev/null ; /bigscoots/wpo_update.sh 2>/dev/null ; wget -O /usr/local/src/centminmod/inc/wpsetup.inc https://raw.githubusercontent.com/jcatello/centminmod/master/inc/wpsetup.inc"; } | crontab -
+	fi
+
+	if ! crontab -l | grep -q wpo_backups_ovz.sh; then 
+		crontab -l | { cat; echo "$(( ( RANDOM % 60 )  + 1 )) $(( ( RANDOM % 4 )  + 1 )) * * * /bigscoots/wpo_backups_ovz.sh"; } | crontab -
+	fi
+
+	if ! crontab -l | grep -q wpo_servicechk.sh; then 
+		crontab -l | { cat; echo "* * * * * /bigscoots/wpo_servicechk.sh >/dev/null 2>&1"; } | crontab -
+	fi
+
+
+	if rpm -qa |grep -q libwebp-devel; then 
+		yum -y remove libwebp-devel
+	fi
+	
+	mkdir -p /bigscoots
+	cd /bigscoots
+	git stash
+	git pull
+	expect /bigscoots/wpo/manage/expect/imagick
+	yum clean all
+	yum update -y --disableplugin=priorities --setopt=deltarpm=0 --enablerepo=remi
+
+fi
