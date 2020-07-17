df1c0192940f2354486e905d2e9ea6f27c026746
Justin catello <justin@bigscoots.com>
2020-07-16 22:42:16 -0400

    Update le.sh

--- wpo/manage/le.sh
+++ wpo/manage/le.sh
@@ -4,25 +4,26 @@
 DOMAIN=$1
 
 if grep -q "${DOMAIN}"-acme.cer /usr/local/nginx/conf/conf.d/"${DOMAIN}".ssl.conf; then
-	echo "SSL already exists."
-	exit 0
-else
-	yes | /usr/local/src/centminmod/addons/acmetool.sh issue "${DOMAIN}" live > /root/.bigscoots/"${DOMAIN}".ssl.txt
-	if grep -q 'issue skipped as ssl cert still valid'; then
-		echo 'SSL already issued, check conf.. will automate this.'
-		exit 0
-	elif
-		grep -q 'Cert success.' /root/.bigscoots/"${DOMAIN}".ssl.txt; then
-		grep "${DOMAIN}"-acme /root/.bigscoots/"${DOMAIN}".ssl.txt |head -3 > /root/.bigscoots/add.txt	
-		sed -i '/ssl_dhparam/r /root/.bigscoots/add.txt' /usr/local/nginx/conf/conf.d/"${DOMAIN}".ssl.conf && rm -f /root/.bigscoots/add.tx /root/.bigscoots/"${DOMAIN}".ssl.txt
-		if nginx -t > /dev/null 2>&1; then 
-			sleep 15
-			npreload > /dev/null 2>&1
-		else 
-			nginx -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail during issueing SSL on ${DOMAIN}  -  $HOSTNAME" monitor@bigscoots.com
-			exit 1
-		fi
-	else 
-		echo " No certs found "
-	fi
+        echo "SSL already exists."
+        exit 0
+else        
+        yes | /usr/local/src/centminmod/addons/acmetool.sh issue "${DOMAIN}" live > /root/.bigscoots/"${DOMAIN}".ssl.txt
+        cat /root/.bigscoots/"${DOMAIN}".ssl.txt
+
+        if grep -q 'issue skipped as ssl cert still valid' /root/.bigscoots/"${DOMAIN}".ssl.txt; then
+                echo 'SSL already issued, check conf.. will automate this.'
+                exit 0
+        elif   grep -q 'Cert success.' /root/.bigscoots/"${DOMAIN}".ssl.txt; then
+               grep "${DOMAIN}"-acme /root/.bigscoots/"${DOMAIN}".ssl.txt |head -3 > /root/.bigscoots/add.txt
+               grep "${DOMAIN}"-acme /root/.bigscoots/"${DOMAIN}".ssl.txt |head -3
+               sed -i '/ssl_dhparam/r /root/.bigscoots/add.txt' /usr/local/nginx/conf/conf.d/"${DOMAIN}".ssl.conf && rm -f /root/.bigscoots/add.tx /root/.bigscoots/"${DOMAIN}".ssl.txt
+               if nginx -t > /dev/null 2>&1; then
+                        npreload > /dev/null 2>&1
+                else
+                        nginx -t 2>&1 | mail -s "WPO URGENT - Nginx conf fail during issueing SSL on ${DOMAIN}  -  $HOSTNAME" monitor@bigscoots.com
+                        exit 1
+                fi
+        else
+                echo " No certs found "
+        fi
 fi
