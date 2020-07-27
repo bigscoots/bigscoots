f090a619cbe7135368a80effdae5b47f2b3121b7
Justin catello <justin@bigscoots.com>
2020-07-27 19:02:35 -0400

    Update wp_cron.sh

--- wpo/extras/wp_cron.sh
+++ wpo/extras/wp_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH=/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/root/bin
-WPCLIFLAGS="--allow-root --skip-plugins --skip-themes --require=/bigscoots/includes/err_report.php"
+WPCLIFLAGS="--allow-root --skip-themes --require=/bigscoots/includes/err_report.php"
 WP_PATH="$1"
 
 wp ${WPCLIFLAGS} cron event run --due-now --path=${WP_PATH}
