3ad5d56cc03da3941afc4b95b947694f7df3aab0
Justin catello <justin@bigscoots.com>
2020-07-30 12:40:33 -0400

    Update wp_cron.sh

--- wpo/extras/wp_cron.sh
+++ wpo/extras/wp_cron.sh
@@ -5,4 +5,4 @@ WP_PATH="$1"
 
 [ $(stat -c "%a" /bin/wp) == "755" ] || chmod 755 /bin/wp
 
-su -s /bin/bash - nginx wp ${WPCLIFLAGS} cron event run --due-now --path=${WP_PATH}
+su -s /bin/bash -l nginx -c "wp ${WPCLIFLAGS} cron event run --due-now --path=${WP_PATH}"
