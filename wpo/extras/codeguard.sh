2ac236c2b89c43e8979a2204e6ad22d576fa7f8d
Justin catello <justin@bigscoots.com>
2020-08-04 16:04:24 -0400

    Update codeguard.sh

--- wpo/extras/codeguard.sh
+++ wpo/extras/codeguard.sh
@@ -4,22 +4,25 @@
 IP=('54.236.209.91' '54.236.233.46' '54.236.233.28' '54.174.91.34' '54.174.153.212' '54.174.115.171')
 website=$1
 
-echo -n "Ente website: "
-read website
 for i in "${IP[@]}"
 do
   	csf -a $i
 done
+
 path=/home/nginx/domains/$website/public
+
 dbname=$(wp --path=$path  --allow-root --skip-plugins --skip-themes config get DB_NAME)
 dbuser=$(wp --path=$path  --allow-root --skip-plugins --skip-themes config get DB_USER)
 dbpass=$(wp --path=$path  --allow-root --skip-plugins --skip-themes config get DB_PASSWORD)
 dbport=${dbport:-3306}
+
 for i in "${IP[@]}" 
 do mysql -e "grant all privileges on "$dbname".* to '$dbuser'@'$i' identified by '$dbpass'"
 done 
+
 sed -i 's=/home/nginx:/sbin/nologin=/home/nginx:/bin/bash=g' /etc/passwd
 userpass=$(</dev/urandom tr -dc '12345qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c32)
+
 echo "$userpass" | passwd --stdin nginx
 echo "nginx user pass: $userpass"
 echo "SSH Host: $(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')"
