596e186190c49e341e5e3533633c9a262bdea5c1
Justin catello <justin@bigscoots.com>
2020-11-25 11:35:21 -0500

    Create dur.sh

---
+++ wpo/manage/dur.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# DUR - https://github.com/amekkawi/diskusagereports
+
+DATE=${date +'%m%d%Y%H%M'}
+DURL="https://${HOSTNAME}/diskusagereports/?${DATE}/"
+
+if [ ! -d /usr/local/nginx/html/diskusagereports ]; then 
+	git clone https://github.com/jcatello/diskusagereports /usr/local/nginx/html/diskusagereports > /dev/null 2>&1
+fi
+
+bash /usr/local/nginx/html/diskusagereports/scripts/find.sh /home/nginx/domains | php -d error_reporting=0 /usr/local/nginx/html/diskusagereports/scripts/process.php /usr/local/nginx/html/diskusagereports/data/"${DATE}" > /dev/null 2>&1
+
+echo { \"disk_usage_url\": "\"${DURL}\"", \"disk_usage_date\": "\"${DATE}\"" }
