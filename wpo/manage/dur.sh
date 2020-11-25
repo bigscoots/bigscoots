3d992e78f7c73ef5405ac5cd6a8293508e84ad11
Justin catello <justin@bigscoots.com>
2020-11-25 11:51:00 -0500

    Update dur.sh

--- wpo/manage/dur.sh
+++ wpo/manage/dur.sh
@@ -9,6 +9,8 @@ if [ ! -d /usr/local/nginx/html/diskusagereports ]; then
 	git clone https://github.com/jcatello/diskusagereports /usr/local/nginx/html/diskusagereports > /dev/null 2>&1
 fi
 
-bash /usr/local/nginx/html/diskusagereports/scripts/find.sh /home/nginx/domains | php -d error_reporting=0 /usr/local/nginx/html/diskusagereports/scripts/process.php /usr/local/nginx/html/diskusagereports/data/"${DATE}" > /dev/null 2>&1
-
-echo { \"disk_usage_url\": "\"${DURL}\"", \"disk_usage_date\": "\"${DATE}\"" }
+if bash /usr/local/nginx/html/diskusagereports/scripts/find.sh /home/nginx/domains | php -d error_reporting=0 /usr/local/nginx/html/diskusagereports/scripts/process.php -q /usr/local/nginx/html/diskusagereports/data/"${DATE}" ; then 
+	echo { \"disk_usage_url\": "\"${DURL}\"", \"disk_usage_date\": "\"${DATE}\"" }
+else
+	echo "Failed to generate diskusagereports"
+fi
